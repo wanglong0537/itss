c07b11cb0ad3398df6ca1e5f9c58787933c2a333
awenhaowc <awenhaowc@gmail.com>
2011-07-08 10:37:42 +0000

    初始导入。

---
+++ 06_Office/src/com/xpsoft/core/jbpm/jpdl/GeometryUtils.java
@@ -0,0 +1,64 @@
+/*    */ package com.xpsoft.core.jbpm.jpdl;
+/*    */ 
+/*    */ import java.awt.Point;
+/*    */ import java.awt.Rectangle;
+/*    */ 
+/*    */ public class GeometryUtils
+/*    */ {
+/*    */   public static double getSlope(int x1, int y1, int x2, int y2)
+/*    */   {
+/* 17 */     return (y2 - y1) / (x2 - x1);
+/*    */   }
+/*    */ 
+/*    */   public static double getYIntercep(int x1, int y1, int x2, int y2)
+/*    */   {
+/* 30 */     return y1 - x1 * getSlope(x1, y1, x2, y2);
+/*    */   }
+/*    */ 
+/*    */   public static Point getRectangleCenter(Rectangle rect)
+/*    */   {
+/* 39 */     return new Point((int)rect.getCenterX(), (int)rect.getCenterY());
+/*    */   }
+/*    */ 
+/*    */   public static Point getRectangleLineCrossPoint(Rectangle rectangle, Point p1, int grow)
+/*    */   {
+/* 50 */     Rectangle rect = rectangle.getBounds();
+/* 51 */     rect.grow(grow, grow);
+/* 52 */     Point p0 = getRectangleCenter(rect);
+/*    */ 
+/* 54 */     if (p1.x == p0.x) {
+/* 55 */       if (p1.y < p0.y) {
+/* 56 */         return new Point(p0.x, rect.y);
+/*    */       }
+/* 58 */       return new Point(p0.x, rect.y + rect.height);
+/*    */     }
+/*    */ 
+/* 61 */     if (p1.y == p0.y) {
+/* 62 */       if (p1.x < p0.x) {
+/* 63 */         return new Point(rect.x, p0.y);
+/*    */       }
+/* 65 */       return new Point(rect.x + rect.width, p0.y);
+/*    */     }
+/*    */ 
+/* 68 */     double slope = getSlope(p0.x, p0.y, rect.x, rect.y);
+/* 69 */     double slopeLine = getSlope(p0.x, p0.y, p1.x, p1.y);
+/* 70 */     double yIntercep = getYIntercep(p0.x, p0.y, p1.x, p1.y);
+/*    */ 
+/* 72 */     if (Math.abs(slopeLine) > slope - 0.01D) {
+/* 73 */       if (p1.y < rect.y) {
+/* 74 */         return new Point((int)Math.round((rect.y - yIntercep) / slopeLine), rect.y);
+/*    */       }
+/* 76 */       return new Point((int)Math.round((rect.y + rect.height - yIntercep) / slopeLine), rect.y + rect.height);
+/*    */     }
+/*    */ 
+/* 79 */     if (p1.x < rect.x) {
+/* 80 */       return new Point(rect.x, (int)Math.round(slopeLine * rect.x + yIntercep));
+/*    */     }
+/* 82 */     return new Point(rect.x + rect.width, (int)Math.round(slopeLine * (rect.x + rect.width) + yIntercep));
+/*    */   }
+/*    */ }
+
+/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
+ * Qualified Name:     com.xpsoft.core.jbpm.jpdl.GeometryUtils
+ * JD-Core Version:    0.6.0
+ */
---
+++ 06_Office/src/com/xpsoft/core/model/OnlineUser.java
@@ -0,0 +1,74 @@
+/*    */ package com.xpsoft.core.model;
+/*    */ 
+/*    */ public class OnlineUser
+/*    */ {
+/*    */   private String sessionId;
+/*    */   private Long userId;
+/*    */   private String username;
+/*    */   private String fullname;
+/*    */   private String depPath;
+/*    */   private String roleIds;
+/*    */   private Short title;
+/*    */ 
+/*    */   public String getSessionId()
+/*    */   {
+/* 45 */     return this.sessionId;
+/*    */   }
+/*    */ 
+/*    */   public void setSessionId(String sessionId) {
+/* 49 */     this.sessionId = sessionId;
+/*    */   }
+/*    */ 
+/*    */   public Long getUserId() {
+/* 53 */     return this.userId;
+/*    */   }
+/*    */ 
+/*    */   public void setUserId(Long userId) {
+/* 57 */     this.userId = userId;
+/*    */   }
+/*    */ 
+/*    */   public String getUsername() {
+/* 61 */     return this.username;
+/*    */   }
+/*    */ 
+/*    */   public void setUsername(String username) {
+/* 65 */     this.username = username;
+/*    */   }
+/*    */ 
+/*    */   public String getFullname() {
+/* 69 */     return this.fullname;
+/*    */   }
+/*    */ 
+/*    */   public void setFullname(String fullname) {
+/* 73 */     this.fullname = fullname;
+/*    */   }
+/*    */ 
+/*    */   public String getDepPath() {
+/* 77 */     return this.depPath;
+/*    */   }
+/*    */ 
+/*    */   public void setDepPath(String depPath) {
+/* 81 */     this.depPath = depPath;
+/*    */   }
+/*    */ 
+/*    */   public String getRoleIds() {
+/* 85 */     return this.roleIds;
+/*    */   }
+/*    */ 
+/*    */   public void setRoleIds(String roleIds) {
+/* 89 */     this.roleIds = roleIds;
+/*    */   }
+/*    */ 
+/*    */   public Short getTitle() {
+/* 93 */     return this.title;
+/*    */   }
+/*    */ 
+/*    */   public void setTitle(Short title) {
+/* 97 */     this.title = title;
+/*    */   }
+/*    */ }
+
+/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
+ * Qualified Name:     com.xpsoft.core.model.OnlineUser
+ * JD-Core Version:    0.6.0
+ */
