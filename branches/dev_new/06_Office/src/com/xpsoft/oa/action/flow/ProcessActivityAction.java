5be86387740bec8ce2d980becc282f4dbfd82cdf
tongjunper <tongjunper@gmail.com>
2011-07-13 11:55:55 +0000

    Comment By Tong

--- branches/dev_new/06_Office/src/com/xpsoft/oa/action/flow/ProcessActivityAction.java
+++ branches/dev_new/06_Office/src/com/xpsoft/oa/action/flow/ProcessActivityAction.java
@@ -316,8 +316,3 @@ import flexjson.JSONSerializer;
 /* 371 */     return info;
    }
  }
-
-/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
- * Qualified Name:     com.xpsoft.oa.action.flow.ProcessActivityAction
- * JD-Core Version:    0.6.0
- */
