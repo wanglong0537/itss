469b405105377d73bb8e49b2623604398111a18f
tongjunper <tongjunper@gmail.com>
2011-07-21 08:38:11 +0000

    add by tongjp 提交导购管理中的类

--- 06_Office_DG/src/com/xpsoft/oa/service/info/impl/NewsCommentServiceImpl.java
+++ 06_Office_DG/src/com/xpsoft/oa/service/info/impl/NewsCommentServiceImpl.java
@@ -1,18 +1,18 @@
-/*    */ package com.xpsoft.oa.service.info.impl;
-/*    */ 
-/*    */ import com.xpsoft.core.service.impl.BaseServiceImpl;
-/*    */ import com.xpsoft.oa.dao.info.NewsCommentDao;
-/*    */ import com.xpsoft.oa.model.info.NewsComment;
-/*    */ import com.xpsoft.oa.service.info.NewsCommentService;
-/*    */ 
-/*    */ public class NewsCommentServiceImpl extends BaseServiceImpl<NewsComment>
-/*    */   implements NewsCommentService
-/*    */ {
-/*    */   private NewsCommentDao dao;
-/*    */ 
-/*    */   public NewsCommentServiceImpl(NewsCommentDao dao)
-/*    */   {
-/* 15 */     super(dao);
-/* 16 */     this.dao = dao;
-/*    */   }
-/*    */ }
+ package com.xpsoft.oa.service.info.impl;
+ 
+ import com.xpsoft.core.service.impl.BaseServiceImpl;
+ import com.xpsoft.oa.dao.info.NewsCommentDao;
+ import com.xpsoft.oa.model.info.NewsComment;
+ import com.xpsoft.oa.service.info.NewsCommentService;
+ 
+ public class NewsCommentServiceImpl extends BaseServiceImpl<NewsComment>
+   implements NewsCommentService
+ {
+   private NewsCommentDao dao;
+ 
+   public NewsCommentServiceImpl(NewsCommentDao dao)
+   {
+     super(dao);
+     this.dao = dao;
+   }
+ }
