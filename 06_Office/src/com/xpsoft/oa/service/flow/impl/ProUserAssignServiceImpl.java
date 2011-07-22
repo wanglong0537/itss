4c35de839753c33d7bc1c7a454aabc93951b4a9f
icemanzp <icemanzp@gmail.com>
2011-07-22 03:17:16 +0000

    Comment By Jack: 格式整理

--- 06_Office/src/com/xpsoft/oa/service/flow/impl/ProUserAssignServiceImpl.java
+++ 06_Office/src/com/xpsoft/oa/service/flow/impl/ProUserAssignServiceImpl.java
@@ -1,33 +1,26 @@
-/*    */ package com.xpsoft.oa.service.flow.impl;
-/*    */ 
-/*    */ import com.xpsoft.core.service.impl.BaseServiceImpl;
-/*    */ import com.xpsoft.oa.dao.flow.ProUserAssignDao;
-/*    */ import com.xpsoft.oa.model.flow.ProUserAssign;
-/*    */ import com.xpsoft.oa.service.flow.ProUserAssignService;
-/*    */ import java.util.List;
-/*    */ 
-/*    */ public class ProUserAssignServiceImpl extends BaseServiceImpl<ProUserAssign>
-/*    */   implements ProUserAssignService
-/*    */ {
-/*    */   private ProUserAssignDao dao;
-/*    */ 
-/*    */   public ProUserAssignServiceImpl(ProUserAssignDao dao)
-/*    */   {
-/* 17 */     super(dao);
-/* 18 */     this.dao = dao;
-/*    */   }
-/*    */ 
-/*    */   public List<ProUserAssign> getByDeployId(String deployId) {
-/* 22 */     return this.dao.getByDeployId(deployId);
-/*    */   }
-/*    */ 
-/*    */   public ProUserAssign getByDeployIdActivityName(String deployId, String activityName)
-/*    */   {
-/* 29 */     return this.dao.getByDeployIdActivityName(deployId, activityName);
-/*    */   }
-/*    */ }
+package com.xpsoft.oa.service.flow.impl;
 
-/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
- * Qualified Name:     com.xpsoft.oa.service.flow.impl.ProUserAssignServiceImpl
- * JD-Core Version:    0.6.0
- */
+import com.xpsoft.core.service.impl.BaseServiceImpl;
+import com.xpsoft.oa.dao.flow.ProUserAssignDao;
+import com.xpsoft.oa.model.flow.ProUserAssign;
+import com.xpsoft.oa.service.flow.ProUserAssignService;
+import java.util.List;
+
+public class ProUserAssignServiceImpl extends BaseServiceImpl<ProUserAssign>
+		implements ProUserAssignService {
+	private ProUserAssignDao dao;
+
+	public ProUserAssignServiceImpl(ProUserAssignDao dao) {
+		super(dao);
+		this.dao = dao;
+	}
+
+	public List<ProUserAssign> getByDeployId(String deployId) {
+		return this.dao.getByDeployId(deployId);
+	}
+
+	public ProUserAssign getByDeployIdActivityName(String deployId,
+			String activityName) {
+		return this.dao.getByDeployIdActivityName(deployId, activityName);
+	}
+}
