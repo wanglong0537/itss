f59f8fff78fb8637052ee4f10f9e004b5fd6a1d9
tongjunper <tongjunper@gmail.com>
2011-07-28 08:29:10 +0000

    add by tongjp 添加webservice接口

---
+++ 06_Office/webservice/TestService.java
@@ -0,0 +1,7 @@
+
+public class TestService {
+	public String helloWord(String s){
+		System.out.println("------------------------"+s+"-------------------------");
+		return s;
+	}
+}
---
+++ 06_Office/webservice/com/xpsoft/webservice/action/login/LoginServie.java
@@ -0,0 +1,5 @@
+package com.xpsoft.webservice.action.login;
+
+public interface LoginServie {
+	public String Login(String userName,String passwd);
+}
