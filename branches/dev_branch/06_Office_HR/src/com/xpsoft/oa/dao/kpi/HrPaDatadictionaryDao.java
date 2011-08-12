61764ab55f8677047fb2ca697baf81d2612e0205
muheye2006 <muheye2006@gmail.com>
2011-08-12 05:57:10 +0000

    add by guansq at 2011-8-12 代码添加详细注释

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/kpi/HrPaDatadictionaryDao.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/kpi/HrPaDatadictionaryDao.java
@@ -6,5 +6,8 @@ import com.xpsoft.core.dao.BaseDao;
 import com.xpsoft.oa.model.kpi.HrPaDatadictionary;
 
 public interface HrPaDatadictionaryDao extends BaseDao<HrPaDatadictionary>{
+	/*
+	 * 分类取得项目考核类型、项目考核频度、项目考核方式关键字
+	 * */
 	public abstract Map<Long, String> getAllByParentId(long parentId);
 }
