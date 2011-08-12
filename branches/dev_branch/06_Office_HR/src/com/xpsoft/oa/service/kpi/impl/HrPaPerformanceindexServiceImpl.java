8fd5e685375b43e3e2bd0ed30ca7f82bb0520839
muheye2006 <muheye2006@gmail.com>
2011-08-12 05:37:13 +0000

    add by guansq at 2011-8-12 考核项目发布之前判断得分是否符合要求

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaPerformanceindexServiceImpl.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaPerformanceindexServiceImpl.java
@@ -1,9 +1,19 @@
 package com.xpsoft.oa.service.kpi.impl;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import com.xpsoft.core.command.QueryFilter;
 import com.xpsoft.core.service.impl.BaseServiceImpl;
+import com.xpsoft.core.util.AppUtil;
 import com.xpsoft.oa.dao.kpi.HrPaPerformanceindexDao;
 import com.xpsoft.oa.model.kpi.HrPaPerformanceindex;
+import com.xpsoft.oa.model.kpi.HrPaPerformanceindexscore;
 import com.xpsoft.oa.service.kpi.HrPaPerformanceindexService;
+import com.xpsoft.oa.service.kpi.HrPaPerformanceindexscoreService;
 
 public class HrPaPerformanceindexServiceImpl extends BaseServiceImpl<HrPaPerformanceindex> 
 	implements HrPaPerformanceindexService {
@@ -14,7 +24,24 @@ public class HrPaPerformanceindexServiceImpl extends BaseServiceImpl<HrPaPerform
 		this.dao = dao;
 	}
 	
-	public void saveToPublish(long piId) {
+	public boolean saveToPublish(long piId) {
+		HrPaPerformanceindexscoreService hrPaPerformanceindexscoreService = 
+				(HrPaPerformanceindexscoreService)AppUtil.getBean("hrPaPerformanceindexscoreService");
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Q_piId_L_EQ", String.valueOf(piId));
+		QueryFilter filter = new QueryFilter(map);
+		List<HrPaPerformanceindexscore> list = hrPaPerformanceindexscoreService.getAll(filter);
+		if(list.size() == 0 || list.size() > 5) {
+			return false;
+		}
+		HashSet<BigDecimal> set = new HashSet<BigDecimal>();
+		for(int i = 0; i < list.size(); i++) {
+			set.add(list.get(i).getPisScore());
+		}
+		if(set.size() < list.size()) {
+			return false;
+		}
 		this.dao.saveToPublish(piId);
+		return true;
 	}
 }
