a11b9eed444493aa3f3d6165fe7c67e70cda1013
muheye2006 <muheye2006@gmail.com>
2011-08-18 01:43:47 +0000

    add by guansq at 2011-8-18 添加KPI考核模板对应的个人考核模板模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaKpiPBC2UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.xpsoft.oa.service.kpi.impl;
+
+import com.xpsoft.core.service.impl.BaseServiceImpl;
+import com.xpsoft.oa.dao.kpi.HrPaKpiPBC2UserDao;
+import com.xpsoft.oa.model.kpi.HrPaKpiPBC2User;
+import com.xpsoft.oa.service.kpi.HrPaKpiPBC2UserService;
+
+public class HrPaKpiPBC2UserServiceImpl extends BaseServiceImpl<HrPaKpiPBC2User>
+		implements HrPaKpiPBC2UserService {
+	private HrPaKpiPBC2UserDao dao;
+	
+	public HrPaKpiPBC2UserServiceImpl(HrPaKpiPBC2UserDao dao) {
+		super(dao);
+		this.dao = dao;
+	}
+}
