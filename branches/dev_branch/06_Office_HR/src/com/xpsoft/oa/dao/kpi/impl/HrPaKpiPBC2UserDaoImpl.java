5bb8cc1e8803718b3b143d36d883e735683c4ee4
muheye2006 <muheye2006@gmail.com>
2011-08-18 01:42:02 +0000

    add by guansq at 2011-8-18 添加KPI考核模板对应的个人考核模板模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/kpi/impl/HrPaKpiPBC2UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.xpsoft.oa.dao.kpi.impl;
+
+import com.xpsoft.core.dao.impl.BaseDaoImpl;
+import com.xpsoft.oa.dao.kpi.HrPaKpiPBC2UserDao;
+import com.xpsoft.oa.model.kpi.HrPaKpiPBC2User;
+
+public class HrPaKpiPBC2UserDaoImpl extends BaseDaoImpl<HrPaKpiPBC2User>
+		implements HrPaKpiPBC2UserDao {
+	public HrPaKpiPBC2UserDaoImpl() {
+		super(HrPaKpiPBC2User.class);
+	}
+}
---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/kpi/impl/HrPaKpiitem2userDaoImpl.java
@@ -0,0 +1,12 @@
+package com.xpsoft.oa.dao.kpi.impl;
+
+import com.xpsoft.core.dao.impl.BaseDaoImpl;
+import com.xpsoft.oa.dao.kpi.HrPaKpiitem2userDao;
+import com.xpsoft.oa.model.kpi.HrPaKpiitem2user;
+
+public class HrPaKpiitem2userDaoImpl extends BaseDaoImpl<HrPaKpiitem2user>
+		implements HrPaKpiitem2userDao {
+	public HrPaKpiitem2userDaoImpl() {
+		super(HrPaKpiitem2user.class);
+	}
+}
