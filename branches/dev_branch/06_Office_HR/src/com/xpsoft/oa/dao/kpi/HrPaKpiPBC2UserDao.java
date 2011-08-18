02928333da14af9004c3a73d48c567e7f66ede4f
muheye2006 <muheye2006@gmail.com>
2011-08-18 01:41:46 +0000

    add by guansq at 2011-8-18 添加KPI考核模板对应的个人考核模板模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/kpi/HrPaKpiPBC2UserDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.kpi;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.kpi.HrPaKpiPBC2User;
+
+public interface HrPaKpiPBC2UserDao extends BaseDao<HrPaKpiPBC2User>  {
+
+}
