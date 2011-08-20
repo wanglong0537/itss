2480701780e064e46c0c589ef9b9455d3f3f4bbf
likang77 <likang77@gmail.com>
2011-08-20 03:33:42 +0000

    加入超市巡检

--- branches/dev_new/OaMiddleware/src/com/xpsoft/spaq/action/FoodSafetyAction.java
+++ branches/dev_new/OaMiddleware/src/com/xpsoft/spaq/action/FoodSafetyAction.java
@@ -54,11 +54,15 @@ import com.xpsoft.framework.web.adapter.struts2.BaseAction;
  */
 public class FoodSafetyAction extends BaseAction{
 	
-	//中间件访问oa地址 webservice
+	//中间件访问 电子台账系统地址
 	public static final String FoodSafetyUrl = PropertiesUtil.getProperties("system.spaq.url");
 	
+	
+	//中间件访问 超市监管系统
+	public static final String CsjgUrl = PropertiesUtil.getProperties("system.csjg.url");
+	
 	/**
-	 * 转发pad端请求到食品安全系统
+	 * 转发pad端请求到电子台账系统
 	 * @Methods Name forwardRequest
 	 * @Create In Aug 2, 2011 By likang
 	 * @return
@@ -70,7 +74,7 @@ public class FoodSafetyAction extends BaseAction{
 		String jsonString = jo.toString();
 		//访问食品安全的url
 		StringBuffer url = new StringBuffer();
-		System.out.println("-------------begin at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
+//		System.out.println("-------------begin at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
 		try {
 			//得到所有参数
 			String paramString = super.getRequest().getParameter("param");
@@ -102,21 +106,71 @@ public class FoodSafetyAction extends BaseAction{
 		pw.append(jsonString);
 		pw.flush();
 		pw.close();
-		System.out.println("-------------end at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
+//		System.out.println("-------------end at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
+		return null;
+	}
+	
+	
+	/**
+	 * 转发pad端请求到超市监管系统
+	 * @Methods Name csjg
+	 * @Create In Aug 2, 2011 By likang
+	 * @return
+	 * @throws Exception String
+	 */
+	public String csjg() throws Exception  {
+		JSONObject jo = new JSONObject();
+		jo.put("success", "false");
+		String jsonString = jo.toString();
+		//访问食品安全的url
+		StringBuffer url = new StringBuffer();
+//		System.out.println("-------------begin at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
+		try {
+			//得到所有参数
+			String paramString = super.getRequest().getParameter("param");
+			//得到target数字
+			String target = super.getRequest().getParameter("target");
+			System.out.println("target:"+target);
+			//解码
+			if (target != null) {
+				String targetUrl = PropertiesUtil.getProperties(target);
+				if (targetUrl != null) {
+					url.append(CsjgUrl);
+					url.append(targetUrl);
+					url.append("?");
+					String paramStringEncode = HttpUtil.encodeParamString(paramString);
+					url.append(paramStringEncode);
+					System.out.println("原参数:" +paramString);
+					System.out.println("转码后:" +paramStringEncode);
+					//读取json
+					jsonString = HttpUtil.getJsonStringByUrl(url.toString());
+				}
+				
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		//输出json
+		PrintWriter pw = super.getPrintWriter();
+		pw.append(jsonString);
+		pw.flush();
+		pw.close();
+//		System.out.println("-------------end at:"+DateUtil.getCurrentDateTimeString()+"---------------------------");
 		return null;
 	}
 	
 	public static void main(String args[]) {
-		String aString = "billCode=晋C0000100009&startDate=20090101&endDate=20110808&pageNum=1&pageSize=15";
+		String aString = "registerCode=140300200010288";
 		try {
 			System.out.println(URLEncoder.encode(aString,"utf-8"));
-			System.out.println(URLDecoder.decode("billCode=%E6%99%8BC0000100009&startDate=20090101&endDate=20110808&pageNum=1&pageSize=15", "utf-8"));
+//			System.out.println(URLDecoder.decode("billCode=%E6%99%8BC0000100009&startDate=20090101&endDate=20110808&pageNum=1&pageSize=15", "utf-8"));
 		
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		System.out.println(HttpUtil.encodeParamString("a=阳泉"));
+//		System.out.println(HttpUtil.encodeParamString(aString));
 //		System.out.println(HttpUtil.encodeParamString("taskid=分发"));
 	}
 	
