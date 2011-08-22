ef1ef20da57fc2da7c2977c575ab62da700d86e6
muheye2006 <muheye2006@gmail.com>
2011-08-22 01:13:21 +0000

    add by guansq at 2011-8-22 完善考核模板同步功能

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaKpiPBC2UserService.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaKpiPBC2UserService.java
@@ -4,5 +4,5 @@ import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.kpi.HrPaKpiPBC2User;
 
 public interface HrPaKpiPBC2UserService extends BaseService<HrPaKpiPBC2User> {
-
+	
 }
---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaKpiitemHistService.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.service.kpi;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.kpi.HrPaKpiitemHist;
+
+public interface HrPaKpiitemHistService extends BaseService<HrPaKpiitemHist>{
+
+}
