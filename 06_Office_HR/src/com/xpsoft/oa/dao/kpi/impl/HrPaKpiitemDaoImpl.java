cffb515fddb9cb535c3d89a467e1eeb638820b2f
muheye2006 <muheye2006@gmail.com>
2011-08-22 01:12:12 +0000

    add by guansq at 2011-8-22 完善考核模板同步功能

--- 06_Office_HR/src/com/xpsoft/oa/dao/kpi/impl/HrPaKpiitemDaoImpl.java
+++ 06_Office_HR/src/com/xpsoft/oa/dao/kpi/impl/HrPaKpiitemDaoImpl.java
@@ -1,5 +1,9 @@
 package com.xpsoft.oa.dao.kpi.impl;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+
 import com.xpsoft.core.dao.impl.BaseDaoImpl;
 import com.xpsoft.oa.dao.kpi.HrPaKpiitemDao;
 import com.xpsoft.oa.model.kpi.HrPaKpiitem;
@@ -8,5 +12,27 @@ public class HrPaKpiitemDaoImpl extends BaseDaoImpl<HrPaKpiitem>
 	implements HrPaKpiitemDao{
 	public HrPaKpiitemDaoImpl(){
 		super(HrPaKpiitem.class);
+	}
+	
+	public boolean findByPiIdAndPbcId(long piId, String[] pbcIds) {
+		String hql = "select count(*) from HrPaKpiitem where pi.id= "
+				+piId + " and pbc.id in (";
+		hql += pbcIds[0];
+		for(int i = 1; i < pbcIds.length; i++) {
+			hql += "," + pbcIds[i];
+		}
+		hql += ")";
+		
+		final String hql2 = hql;
+		
+		long flag = (Long)this.getHibernateTemplate().execute(
+			new HibernateCallback() {
+				public Object doInHibernate(Session session) {
+					Query query = session.createQuery(hql2);
+					return query.list().get(0);
+				}
+			}
+		);
+		return flag > 0 ? true : false;
 	}
 }
