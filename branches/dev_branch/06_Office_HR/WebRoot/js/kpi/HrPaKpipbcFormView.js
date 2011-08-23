024b2c549bc1a128dd4a6ad343eb9fdf511b2038
muheye2006 <muheye2006@gmail.com>
2011-08-23 05:13:21 +0000

    add by guansq at 2011-8-23 增加考核模板预览功能

--- branches/dev_branch/06_Office_HR/WebRoot/js/kpi/HrPaKpipbcFormView.js
+++ branches/dev_branch/06_Office_HR/WebRoot/js/kpi/HrPaKpipbcFormView.js
@@ -1,8 +1,5 @@
 HrPaKpipbcFormView = Ext.extend(Ext.Window, {
-	formPanel : null,
-	gridPanel : null,
-	store : null,
-	topbar : null,
+	panel : null,
 	constructor : function(a) {
 		if(a == null) {
 			a = {};
@@ -10,203 +7,25 @@ HrPaKpipbcFormView = Ext.extend(Ext.Window, {
 		Ext.apply(this, a);
 		this.initComponents();
 		HrPaKpipbcFormView.superclass.constructor.call(this, {
-			id : "HrPaKpipbcFormViewWin",
+			id : "HrPaKpipbcFormView",
 			region : "center",
-			layout : "border",
+			layout : "fit",
+			border : false,
 			items : [
-				this.formPanel,
-				this.gridPanel
+				this.panel
 			],
 			modal : true,
 			height : 600,
 			width : 850,
 			autoScroll : true,
-			title : "考核模板查看",
-			buttonAlign : "center",
-			buttons : this.buttons
+			title : "考核模板预览"
 		});
 	},
 	initComponents : function() {
-		this.formPanel = new Ext.FormPanel({
-			region : "north",
-			height : 100,
-			id : "HrPaKpipbcFormView",
-			bodyStyle : "padding:20px 20px 20px 20px",
-			frame : false,
+		this.panel = new Ext.Panel({
 			border : false,
-			layout : "form",
-			layoutConfig : {
-				padding : "5",
-				align : "middle"
-			},
-			id : "HrPaKpipbcForm",
-			reader : new Ext.data.JsonReader({
-				root : "data"
-			},[
-				{
-					name : "hrPaKpipbc.pbcName",
-					mapping : "pbcName"
-				}, {
-					name : "hrPaKpipbc.belongDept.depId",
-					mapping : "belongDept.depId"
-				}, {
-					name : "hrPaKpipbc.belongDept.depName",
-					mapping : "belongDept.depName"
-				}, {
-					name : "hrPaKpipbc.belongPost.jobName",
-					mapping : "belongPost.jobName"
-				}, {
-					name : "hrPaKpipbc.frequency.name",
-					mapping : "frequency.name"
-				}
-			]),
-			items : [ 
-				{
-					xtype : "container",
-					border : false,
-					layout : "column",
-					items : [
-						{
-							layout : "form",
-							border : false,
-							columnWidth : 0.5,
-							items : [
-								{
-									fieldLabel : "考核模板名称",
-									name : "hrPaKpipbc.pbcName",
-									id : "pbcName",
-									xtype : "textfield",
-									width : 165,
-									readOnly : true,
-									allowBlank : false,
-									blankText : "考核模板名称不能为空！"
-								}, {
-									fieldLabel : "所属部门",
-									name : "hrPaKpipbc.belongDept.depName",
-									id : "depName",
-									xtype : "textfield",
-									width : 165,
-									readOnly : true
-								}
-							]
-						}, {
-							layout : "form",
-							border : false,
-							columnWidth : 0.5,
-							items : [
-								{
-									fieldLabel : "考核频度",
-									name : "hrPaKpipbc.frequency.name",
-									xtype : "textfield",
-									readOnly : true
-								}, {
-									fieldLabel : "所属岗位",
-									name : "hrPaKpipbc.belongPost.jobName",
-									xtype : "textfield",
-									readOnly : true
-								}
-							]
-						}
-					]
-				}
-			]
+			bodyBorder : false,
+			html : "<iframe frameborder='no' width=100% height=100% src='" + __ctxPath + "/kpi/previewHrPaKpipbc.do?id=" + this.pbcId + "'>"
 		});
-		var pbcId = (this.pbcId == null || this.pbcId == "undefined") ? 0 : this.pbcId;
-		this.store = new Ext.data.JsonStore({
-			url : __ctxPath + "/kpi/listHrPaKpiitem.do?Q_pbc.id_L_EQ=" + pbcId,
-			id : "id",
-			root : "result",
-			remoteSort : true,
-			fields : [
-				{
-					name : "id",
-					type : "int"
-				}, {
-					name : "piId",
-					mapping : "pi.id"
-				}, {
-					name : "pi.paName",
-					mapping : "pi.paName"
-				},
-				"weight"
-			]
-		});
-		this.store.setDefaultSort("id", "asc");
-		this.store.load({
-			params : {
-				start : 0,
-				limit : 100
-			}
-		});
-		var c = new Ext.grid.CheckboxSelectionModel();
-		var a = new Ext.grid.ColumnModel({
-			columns : [
-				c,
-				new Ext.grid.RowNumberer(),
-				{
-					header : "id",
-					dataIndex : "id",
-					hidden : true
-				}, {
-					header : "piId",
-					dataIndex : "piId",
-					hidden : true
-				}, {
-					header : "考核指标名称",
-					dataIndex : "pi.paName",
-					width : 250
-				}, {
-					header : "权重",
-					dataIndex : "weight",
-					editor : new Ext.form.Field({
-						xtype : "textfield"
-					})
-				}
-			],
-			defaults : {
-				sortable : true,
-				menuDisable : false
-			}
-		});
-		this.gridPanel = new Ext.grid.GridPanel({
-			id : "HrPaKpiitemGrid",
-			region : "center",
-			autoWidth : true,
-			autoHeight : true,
-			stripeRows : true,
-			autoScroll : true,
-			closeable : true,
-			store : this.store,
-			trackMouseOver : true,
-			disableSelection : false,
-			loadMask : true,
-			cm : a,
-			sm : c,
-			viewConfig : {
-				forceFit : true,
-				enableRowBody : false,
-				showPreview : false
-			},
-			bbar : new Ext.PagingToolbar({
-				pageSize : 100,
-				store : this.store,
-				displayInfo : true,
-				displayMsg : "当前显示{0}至{1}，共{2}条记录",
-				emptyMsg : "当前没有记录"
-			})
-		});
-		if(this.pbcId != null && this.pbcId != "undefined") {
-			this.formPanel.getForm().load({
-				deferredRender : false,
-				url : __ctxPath + "/kpi/getHrPaKpipbc.do?id=" + this.pbcId,
-				waitMsg : "正在载入数据……",
-				success : function(f, d) {
-					
-				},
-				failure : function() {
-					
-				}
-			});
-		}
 	}
 });
