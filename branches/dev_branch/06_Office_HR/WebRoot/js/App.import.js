58bc9868a100a9742307e21295356b4421cc54f9
awenhaowc <awenhaowc@gmail.com>
2011-08-29 10:06:54 +0000

    add by awen

--- branches/dev_branch/06_Office_HR/WebRoot/js/App.import.js
+++ branches/dev_branch/06_Office_HR/WebRoot/js/App.import.js
@@ -397,4 +397,6 @@ App.importJs = {
 			__ctxPath + "/js/kpi/AuthorizePbcForm.js"],
 	GradeAuthorizePbcView : [__ctxPath + "/js/kpi/GradeAuthorizePbcView.js",
 			__ctxPath + "/js/kpi/GradeAuthorizePbcForm.js"]
+	RewardsPunishmentsTypeView : [ __ctxPath + "/js/rewardsPunishments/RewardsPunishmentsTypeView.js",
+			                    __ctxPath + "/js/rewardsPunishments/RewardsPunishmentsTypeForm.js" ]
 };
