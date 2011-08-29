e5dc877c0a94e373507b1b3b1d4310b3b3a95455
muheye2006 <muheye2006@gmail.com>
2011-08-29 04:49:35 +0000

    add by guansq at 2011-8-29 增加KPI考核打分模块

--- 06_Office_HR/src/com/xpsoft/oa/action/kpi/HrPaAuthorizepbcAction.java
+++ 06_Office_HR/src/com/xpsoft/oa/action/kpi/HrPaAuthorizepbcAction.java
@@ -1,10 +1,26 @@
 package com.xpsoft.oa.action.kpi;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
+import com.xpsoft.core.command.QueryFilter;
+import com.xpsoft.core.util.AppUtil;
+import com.xpsoft.core.util.ContextUtil;
 import com.xpsoft.core.web.action.BaseAction;
 import com.xpsoft.oa.model.kpi.HrPaAuthorizepbc;
+import com.xpsoft.oa.model.kpi.HrPaAuthpbccitem;
+import com.xpsoft.oa.model.kpi.HrPaKpiPBC2User;
+import com.xpsoft.oa.model.kpi.HrPaPerformanceindexscore;
+import com.xpsoft.oa.model.system.AppUser;
 import com.xpsoft.oa.service.kpi.HrPaAuthorizepbcService;
+import com.xpsoft.oa.service.kpi.HrPaAuthpbccitemService;
+
+import flexjson.JSONSerializer;
 
 public class HrPaAuthorizepbcAction extends BaseAction{
 	@Resource
@@ -36,13 +52,120 @@ public class HrPaAuthorizepbcAction extends BaseAction{
 		return "success";
 	}
 	
+	public String currentList() {
+		AppUser currentUser = ContextUtil.getCurrentUser();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Q_authorTo.userId_L_EQ", String.valueOf(currentUser.getUserId()));
+		QueryFilter filter = new QueryFilter(map);
+		List<HrPaAuthorizepbc> list = this.hrPaAuthorizepbcService.getAll(filter);
+		
+		StringBuffer buff = new StringBuffer("{success:true,'totalCounts':")
+				.append(filter.getPagingBean().getTotalItems()).append(",result:");
+		JSONSerializer json = new JSONSerializer();
+		buff.append(json.exclude(new String[] {}).serialize(list));
+		buff.append("}");
+		this.jsonString = buff.toString();
+		
+		return "success";
+	}
+	
 	public String get(){
 		return "success";
 	}
 	
+	@SuppressWarnings("unchecked")
+	public String preview() {
+		this.hrPaAuthorizepbc = this.hrPaAuthorizepbcService.get(this.id);
+		//拼装SQL语句取出id, weight和关联的paName
+		String sql = "select a.id, a.weight, c.id as piId, c.paName from hr_pa_authpbcitem a, hr_pa_kpiitem2user b, " +
+				"hr_pa_performanceindex c where a.apbcId = " + this.id + " and a.akpiItem2uId = b.id and b.piId = c.id";
+		Map<Map<String, Object>, List<Map<String, Object>>> itemMap = new HashMap<Map<String,Object>, List<Map<String,Object>>>();
+		List<Map<String, Object>> authorItemList = this.hrPaAuthorizepbcService.findDataList(sql);
+		for(int i = 0; i < authorItemList.size(); i++) {
+			String sql2 = "select id, pisScore, pisDesc from hr_pa_performanceindexscore where piId = " + 
+					authorItemList.get(i).get("piId").toString();
+			List<Map<String, Object>> pisList = this.hrPaAuthorizepbcService.findDataList(sql2);
+			itemMap.put(authorItemList.get(i), pisList);
+		}
+		
+		this.getRequest().setAttribute("itemMap", itemMap);
+		
+		return "show";
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String gridScore() {
+		try {
+			//取得pbc
+			String pbcId = this.getRequest().getParameter("pbcId");
+			this.hrPaAuthorizepbc = this.hrPaAuthorizepbcService.get(Long.parseLong(pbcId));
+			//取得pbc关联的考核项
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("Q_authorPbc.id_L_EQ", pbcId);
+			QueryFilter filter = new QueryFilter(map);
+			HrPaAuthpbccitemService hrPaAuthpbccitemService = (HrPaAuthpbccitemService)AppUtil.getBean("hrPaAuthpbccitemService");
+			List<HrPaAuthpbccitem> itemList = hrPaAuthpbccitemService.getAll(filter);
+			//填入打分结果
+			for(int i = 0; i < itemList.size(); i++) {
+				HrPaAuthpbccitem item = itemList.get(i);
+				Double resultOld = item.getResult();
+				Double resultSubmit = Double.parseDouble(this.getRequest().getParameter(String.valueOf(item.getId())));
+				Double resultNew = 0.0;
+				//判断是否多次打分
+				if(resultOld == 0) {
+					resultNew = resultSubmit;
+				} else {
+					resultNew = (resultOld + resultSubmit) / 2;
+				}
+				itemList.get(i).setResult(resultNew);
+			}
+			//插入数据库
+			hrPaAuthpbccitemService.multiSave(itemList);
+			//返回成功标记
+			this.getRequest().setAttribute("flag", "1");
+		} catch(Exception e) {
+			e.printStackTrace();
+			this.getRequest().setAttribute("flag", "0");
+		}
+		
+		return "gradeResult";
+	}
+	
 	public String save(){
-		this.hrPaAuthorizepbcService.save(this.hrPaAuthorizepbc);
+		Date currentDate = new Date();
+		AppUser currentUser = ContextUtil.getCurrentUser();
+		//获取表单信息
+		long userId = Long.parseLong(this.getRequest().getParameter("userId"));
+		long pbcId = Long.parseLong(this.getRequest().getParameter("pbcId"));
+		String[] authorPbcItems = this.getRequest().getParameter("authorItems").trim().split(" ");
+		//保存授权PBC基本信息
+		HrPaAuthorizepbc authorPbc = new HrPaAuthorizepbc();
+		AppUser authorTo = new AppUser(userId);
+		HrPaKpiPBC2User userPbc = new HrPaKpiPBC2User();
+		userPbc.setId(pbcId);
+		authorPbc.setAuthorTo(authorTo);
+		authorPbc.setUserPbc(userPbc);
+		authorPbc.setAuthDate(currentDate);
+		authorPbc.setAuthPerson(currentUser);
+		//插入数据库
+		HrPaAuthorizepbc authorPbcNew = this.hrPaAuthorizepbcService.save(authorPbc);
+		//保存授权PBC关联的考核指标信息
+		HrPaAuthpbccitemService hrPaAuthpbccitemService = (HrPaAuthpbccitemService)AppUtil.getBean("hrPaAuthpbccitemService");
+		List<HrPaAuthpbccitem> itemList = new ArrayList<HrPaAuthpbccitem>();
+		for(int i = 0; i < authorPbcItems.length; i++) {
+			String[] properties = authorPbcItems[i].trim().split(",");
+			HrPaAuthpbccitem item = new HrPaAuthpbccitem();
+			item.setAuthorPbc(authorPbcNew);
+			item.setAkpiItem2uId(Long.parseLong(properties[0]));
+			item.setWeight(Double.parseDouble(properties[1]));
+			item.setResult(new Double(0));//得分默认为0
+			itemList.add(item);
+		}
+		//批量插入数据库
+		hrPaAuthpbccitemService.multiSave(itemList);
+		
 		this.jsonString = new String("{success:true}");
+		
 		return "success";
 	}
 }
