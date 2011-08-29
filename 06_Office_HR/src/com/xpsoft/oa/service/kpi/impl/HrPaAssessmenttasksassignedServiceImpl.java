a2d807003254308f6c292975b42e93bcfc123f02
muheye2006 <muheye2006@gmail.com>
2011-08-29 04:53:20 +0000

    add by guansq at 2011-8-29 增加KPI考核打分模块

--- 06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaAssessmenttasksassignedServiceImpl.java
+++ 06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaAssessmenttasksassignedServiceImpl.java
@@ -1,5 +1,8 @@
 package com.xpsoft.oa.service.kpi.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import com.xpsoft.core.service.impl.BaseServiceImpl;
 import com.xpsoft.oa.dao.kpi.HrPaAssessmenttasksassignedDao;
 import com.xpsoft.oa.model.kpi.HrPaAssessmenttasksassigned;
@@ -12,5 +15,33 @@ public class HrPaAssessmenttasksassignedServiceImpl extends BaseServiceImpl<HrPa
 	public HrPaAssessmenttasksassignedServiceImpl(HrPaAssessmenttasksassignedDao dao){
 		super(dao);
 		this.dao = dao;
+	}
+	/*
+	 * 批量保存数据
+	 * params: 数据集，数据保存类型
+	 * */
+	public void multiSave(List<HrPaAssessmenttasksassigned> list, String type) {
+		if(list.size() > 0) {
+			for(int i = 0; i < list.size(); i++) {
+				if("category".equals(type)) {
+					String sql = "select id from hr_pa_assessmenttasksassigned " +
+							"where category = " + list.get(i).getCategory() + " and " +
+									"acId = " + list.get(i).getAcId();
+					List<Map<String, Object>> countList = this.findDataList(sql);
+					if(countList.size() > 0) {
+						list.get(i).setId(Long.parseLong(countList.get(0).get("id").toString()));
+					}
+				} else if("user".equals(type)) {
+					String sql = "select id from hr_pa_assessmenttasksassigned " +
+							"where userId = " + list.get(i).getUserId() + " and " +
+									"acId = " + list.get(i).getAcId();
+					List<Map<String, Object>> countList = this.findDataList(sql);
+					if(countList.size() > 0) {
+						list.get(i).setId(Long.parseLong(countList.get(0).get("id").toString()));
+					}
+				}
+				this.save(list.get(i));
+			}
+		}
 	}
 }
--- 06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaAuthpbccitemServiceImpl.java
+++ 06_Office_HR/src/com/xpsoft/oa/service/kpi/impl/HrPaAuthpbccitemServiceImpl.java
@@ -1,5 +1,7 @@
 package com.xpsoft.oa.service.kpi.impl;
 
+import java.util.List;
+
 import com.xpsoft.core.service.impl.BaseServiceImpl;
 import com.xpsoft.oa.dao.kpi.HrPaAuthpbccitemDao;
 import com.xpsoft.oa.model.kpi.HrPaAuthpbccitem;
@@ -12,5 +14,15 @@ public class HrPaAuthpbccitemServiceImpl extends BaseServiceImpl<HrPaAuthpbccite
 	public HrPaAuthpbccitemServiceImpl(HrPaAuthpbccitemDao dao){
 		super(dao);
 		this.dao = dao;
+	}
+	/*
+	 * 批量保存数据
+	 * */
+	public void multiSave(List<HrPaAuthpbccitem> list) {
+		if(list.size() > 0) {
+			for(int i = 0; i < list.size(); i++) {
+				this.save(list.get(i));
+			}
+		}
 	}
 }
