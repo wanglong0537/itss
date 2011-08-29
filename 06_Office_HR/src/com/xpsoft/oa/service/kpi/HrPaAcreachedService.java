3a89780cd6ea66e594ff58d5439b5e319d3ab6a9
muheye2006 <muheye2006@gmail.com>
2011-08-29 04:51:31 +0000

    add by guansq at 2011-8-29 增加KPI考核打分模块

--- 06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaAcreachedService.java
+++ 06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaAcreachedService.java
@@ -1,8 +1,14 @@
 package com.xpsoft.oa.service.kpi;
 
+import java.util.List;
+
 import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.kpi.HrPaAcreached;
 
 public interface HrPaAcreachedService extends BaseService<HrPaAcreached>{
-
+	/*
+	 * 批量保存数据
+	 * params: 数据集，数据保存类型
+	 * */
+	public void multiSave(List<HrPaAcreached> list, String type);
 }
