4ab2018516880184f4847d9e6c62e5b4b5d119ef
muheye2006 <muheye2006@gmail.com>
2011-08-30 07:23:01 +0000

    add by guansq at 2011-8-30 修改考核指标模块保存逻辑

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaPerformanceindexscoreService.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaPerformanceindexscoreService.java
@@ -1,11 +1,18 @@
 package com.xpsoft.oa.service.kpi;
 
+import java.util.List;
+
 import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.kpi.HrPaPerformanceindexscore;
+import com.xpsoft.oa.model.kpi.HrPaPisrule;
 
 public interface HrPaPerformanceindexscoreService extends BaseService<HrPaPerformanceindexscore>{
 	/*
 	 * 通过外键piId批量删除考核指标关联的得分
 	 * */
 	public abstract void removeByPiId(long piId);
+	/*
+	 * 批量添加考核项目关联的分数
+	 * */
+	public void multiSave(List<HrPaPerformanceindexscore> scoreList, List<HrPaPisrule> ruleList, long paMode);
 }
