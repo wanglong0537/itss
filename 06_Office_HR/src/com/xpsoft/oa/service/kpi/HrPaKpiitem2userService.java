fc59109f4c4004ae594d14a1c76e03614b49b62d
muheye2006 <muheye2006@gmail.com>
2011-08-31 07:04:28 +0000

    add by guansq at 2011-8-31 增加计算总分功能

--- 06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaKpiitem2userService.java
+++ 06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaKpiitem2userService.java
@@ -4,5 +4,16 @@ import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.kpi.HrPaKpiitem2user;
 
 public interface HrPaKpiitem2userService extends BaseService<HrPaKpiitem2user> {
-
+	/*
+	 * 计算个人考核模板定性考核项的多人多次打分平均分数
+	 * @param itemId
+	 * 考核项ID
+	 * */
+	public void calculateAvg(Long itemId);
+	/*
+	 * 批量计算考核模板关联所有考核项平均分
+	 * @param pbcId
+	 * 考核模板ID
+	 * */
+	public void multiCal(Long pbcId);
 }
