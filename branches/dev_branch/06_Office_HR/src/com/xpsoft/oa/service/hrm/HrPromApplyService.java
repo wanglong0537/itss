5306ab3de625ce065841c37ba4aad5dcf16b1a3e
muheye2006 <muheye2006@gmail.com>
2011-09-06 06:30:33 +0000

    add by guansq at 2011-9-6 添加人员晋升模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/hrm/HrPromApplyService.java
@@ -0,0 +1,9 @@
+package com.xpsoft.oa.service.hrm;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.hrm.HrPromApply;
+import com.xpsoft.oa.model.hrm.HrPromAssessment;
+
+public interface HrPromApplyService extends BaseService<HrPromApply>{
+
+}
---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/hrm/HrPromAssessmentService.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.service.hrm;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.hrm.HrPromAssessment;
+
+public interface HrPromAssessmentService extends BaseService<HrPromAssessment>{
+
+}
