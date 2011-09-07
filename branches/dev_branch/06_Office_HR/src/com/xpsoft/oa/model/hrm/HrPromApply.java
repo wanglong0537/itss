efc2c9c35d846c46f6d572fb326609a7d5f9353d
muheye2006 <muheye2006@gmail.com>
2011-09-07 07:36:17 +0000

    add by guansq at 2011-9-7 添加人员晋升模块

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/model/hrm/HrPromApply.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/model/hrm/HrPromApply.java
@@ -7,17 +7,23 @@ import com.xpsoft.oa.model.system.AppUser;
 
 public class HrPromApply extends BaseModel{
 	protected Long id;
-	protected AppUser applyUser;
-	protected Job applyPosition;
-	protected Date applyDate;
-	protected String applyReason;
-	protected String target1;
-	protected String target2;
-	protected String target3;
-	protected String intRecord;
-	protected Long postManagerId;
-	protected String postManagerName;
-	protected Integer publishStatus;
+	protected AppUser applyUser;//晋升员工
+	protected Long depId;//所属部门ID
+	protected String depName;//所属部门名称
+	protected Long nowPositionId;//现职位ID
+	protected String nowPositionName;//现职位名称
+	protected String workYear;//工作年限
+	protected String workHereYear;//本单位工作年限
+	protected Job applyPosition;//拟担任职位
+	protected Date applyDate;//拟晋升时间
+	protected String applyReason;//拟晋升原因
+	protected String target1;//工作目标一
+	protected String target2;//工作目标二
+	protected String target3;//工作目标三
+	protected String intRecord;//员工面谈记录
+	protected Long postManagerId;//部门领导ID
+	protected String postManagerName;//部门领导姓名
+	protected Integer publishStatus;//状态 0：草稿 1：审核中 2：退回 3：审核通过 4：删除标记
 	protected AppUser createPerson;
 	protected Date createDate;
 	protected AppUser modifyPerson;
@@ -39,6 +45,54 @@ public class HrPromApply extends BaseModel{
 
 	public void setApplyUser(AppUser applyUser) {
 		this.applyUser = applyUser;
+	}
+
+	public Long getDepId() {
+		return depId;
+	}
+
+	public void setDepId(Long depId) {
+		this.depId = depId;
+	}
+
+	public String getDepName() {
+		return depName;
+	}
+
+	public void setDepName(String depName) {
+		this.depName = depName;
+	}
+
+	public Long getNowPositionId() {
+		return nowPositionId;
+	}
+
+	public void setNowPositionId(Long nowPositionId) {
+		this.nowPositionId = nowPositionId;
+	}
+
+	public String getNowPositionName() {
+		return nowPositionName;
+	}
+
+	public void setNowPositionName(String nowPositionName) {
+		this.nowPositionName = nowPositionName;
+	}
+
+	public String getWorkYear() {
+		return workYear;
+	}
+
+	public void setWorkYear(String workYear) {
+		this.workYear = workYear;
+	}
+
+	public String getWorkHereYear() {
+		return workHereYear;
+	}
+
+	public void setWorkHereYear(String workHereYear) {
+		this.workHereYear = workHereYear;
 	}
 
 	public Job getApplyPosition() {
@@ -97,6 +151,22 @@ public class HrPromApply extends BaseModel{
 		this.intRecord = intRecord;
 	}
 
+	public Long getPostManagerId() {
+		return postManagerId;
+	}
+
+	public void setPostManagerId(Long postManagerId) {
+		this.postManagerId = postManagerId;
+	}
+
+	public String getPostManagerName() {
+		return postManagerName;
+	}
+
+	public void setPostManagerName(String postManagerName) {
+		this.postManagerName = postManagerName;
+	}
+
 	public Integer getPublishStatus() {
 		return publishStatus;
 	}
@@ -135,22 +205,6 @@ public class HrPromApply extends BaseModel{
 
 	public void setModifyDate(Date modifyDate) {
 		this.modifyDate = modifyDate;
-	}
-
-	public Long getPostManagerId() {
-		return postManagerId;
-	}
-
-	public void setPostManagerId(Long postManagerId) {
-		this.postManagerId = postManagerId;
-	}
-
-	public String getPostManagerName() {
-		return postManagerName;
-	}
-
-	public void setPostManagerName(String postManagerName) {
-		this.postManagerName = postManagerName;
 	}
 	
 }
