4eec29d6b34be9b205feb741104a4a7196990e8e
muheye2006 <muheye2006@gmail.com>
2011-09-08 03:58:51 +0000

    add by guansq at 2011-9-8 添加人员晋升模块

--- 06_Office_HR/src/com/xpsoft/oa/action/hrm/HrPromAssessmentAction.java
+++ 06_Office_HR/src/com/xpsoft/oa/action/hrm/HrPromAssessmentAction.java
@@ -1,11 +1,20 @@
 package com.xpsoft.oa.action.hrm;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import com.xpsoft.core.command.QueryFilter;
 import com.xpsoft.core.web.action.BaseAction;
+import com.xpsoft.oa.model.hrm.EmpProfile;
 import com.xpsoft.oa.model.hrm.HrPromAssessment;
 import com.xpsoft.oa.service.hrm.HrPromAssessmentService;
 
 public class HrPromAssessmentAction extends BaseAction{
 	private HrPromAssessment hrPromAssessment;
+	@Resource
 	private HrPromAssessmentService hrPromAssessmentService;
 	private Long id;
 	
@@ -31,6 +40,13 @@ public class HrPromAssessmentAction extends BaseAction{
 	
 	public String list() {
 		return "success";
+	}
+	
+	public String preview() {
+		if(this.id != 0) {
+			this.hrPromAssessment = this.hrPromAssessmentService.get(this.id);
+		}
+		return "show";
 	}
 	
 	public String get() {
