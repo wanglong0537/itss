d3482d683ff1d43dc8e33041ff5da02ed89943c0
muheye2006 <muheye2006@gmail.com>
2011-09-09 03:51:49 +0000

    add by guansq at 2011-9-9 增加绩效系数字段

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaAcreachedService.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaAcreachedService.java
@@ -10,5 +10,5 @@ public interface HrPaAcreachedService extends BaseService<HrPaAcreached>{
 	 * 批量保存数据
 	 * params: 数据集，数据保存类型
 	 * */
-	public void multiSave(List<HrPaAcreached> list, String type);
+	public void multiSave(List<HrPaAcreached> list, String templateId);
 }
