f010c9bff098e9111567cfb232549b3e324d7404
muheye2006 <muheye2006@gmail.com>
2011-09-22 07:33:15 +0000

    add by guansq at 2011-9-22 添加转正管理模块

---
+++ 06_Office_HR/src/com/xpsoft/oa/dao/hrm/HrPostApplyDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.hrm;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.hrm.HrPostApply;
+
+public interface HrPostApplyDao extends BaseDao<HrPostApply>{
+
+}
