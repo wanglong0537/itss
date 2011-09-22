a5602cd8f571c44165039b393d4b4d587d143416
muheye2006 <muheye2006@gmail.com>
2011-09-22 07:34:54 +0000

    add by guansq at 2011-9-22 添加转正管理模块

---
+++ 06_Office_HR/src/com/xpsoft/oa/service/hrm/HrPostApplyService.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.service.hrm;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.hrm.HrPostApply;
+
+public interface HrPostApplyService extends BaseService<HrPostApply>{
+
+}
