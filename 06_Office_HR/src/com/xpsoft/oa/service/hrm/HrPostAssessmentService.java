1d197c3d8f1d7628e0aba2fc20ca69525cb85ba0
muheye2006 <muheye2006@gmail.com>
2011-09-23 09:55:34 +0000

    add by guansq at 2011-9-23 添加人员转正流程

--- 06_Office_HR/src/com/xpsoft/oa/service/hrm/HrPostAssessmentService.java
+++ 06_Office_HR/src/com/xpsoft/oa/service/hrm/HrPostAssessmentService.java
@@ -2,7 +2,10 @@ package com.xpsoft.oa.service.hrm;
 
 import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.hrm.HrPostAssessment;
+import com.xpsoft.oa.model.hrm.HrPromAssessment;
 
 public interface HrPostAssessmentService extends BaseService<HrPostAssessment>{
-
+	public HrPostAssessment getByApplyId(Long applyId);
+	
+	public HrPostAssessment saveViewByApplyId(Long applyId);
 }
