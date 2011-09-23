c0009447eeaf7c2fddc32c0349da35df74887936
muheye2006 <muheye2006@gmail.com>
2011-09-23 09:56:12 +0000

    add by guansq at 2011-9-23 添加人员转正流程

---
+++ 06_Office_HR/WebRoot/js/hrm/HrPostAssessmentForm.js
@@ -0,0 +1,37 @@
+HrPostAssessmentForm = Ext.extend(Ext.Window, {
+	panel : null,
+	constructor : function(a) {
+		if(a == null) {
+			a = {};
+		}
+		Ext.apply(this, a);
+		this.initComponents();
+		HrPostAssessmentForm.superclass.constructor.call(this, {
+			id : "HrPostAssessmentForm",
+			region : "center",
+			layout : "fit",
+			border : false,
+			maximizable : true,
+			items : [
+				this.panel
+			],
+			modal : true,
+			height : 600,
+			width : 850,
+			autoScroll : true,
+			title : "试用期评估页面",
+			listeners : {
+				close : function() {
+
+				}
+			}
+		});
+	},
+	initComponents : function() {
+		this.panel = new Ext.Panel({
+			border : false,
+			bodyBorder : false,
+			html : "<iframe frameborder='no' width=100% height=100% src='" + __ctxPath + "/hrm/getViewByApplyIdHrPostAssessment.do?applyId=" + this.applyId + "&assess=" + this.assess +  "&stamp=" + Math.random() +"'>"
+		});
+	}
+});
