7ff17c44e182051eade453d1f5f01f5e5eb85547
muheye2006 <muheye2006@gmail.com>
2011-09-26 11:40:03 +0000

    add by guansq at 2011-9-26 添加人员转正流程

--- 06_Office_HR/WebRoot/js/hrm/HrPostApplyStatusView.js
+++ 06_Office_HR/WebRoot/js/hrm/HrPostApplyStatusView.js
@@ -57,6 +57,27 @@ HrPostApplyStatusView = Ext.extend(Ext.Panel, {
 			width : 80,
 			actions : b
 		});
+		Date.prototype.format = function(format){ 
+			var o = { 
+				"M+" : this.getMonth()+1, //month 
+				"d+" : this.getDate(), //day 
+				"h+" : this.getHours(), //hour 
+				"m+" : this.getMinutes(), //minute 
+				"s+" : this.getSeconds(), //second 
+				"q+" : Math.floor((this.getMonth()+3)/3), //quarter 
+				"S" : this.getMilliseconds() //millisecond 
+			}
+			if(/(y+)/.test(format)) { 
+				format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+			} 
+			
+			for(var k in o) { 
+				if(new RegExp("("+ k +")").test(format)) { 
+					format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length)); 
+				} 
+			}
+			return format; 
+		}
 		var c = new Ext.grid.CheckboxSelectionModel();
 		var a = new Ext.grid.ColumnModel({
 			columns : [
@@ -76,7 +97,7 @@ HrPostApplyStatusView = Ext.extend(Ext.Panel, {
 					header : "入职时间",
 					dataIndex : "accessionTime",
 					renderer : function(c) {
-						return c;
+						return new Date(c).format("yyyy-MM-dd");
 					}
 				}, {
 					header : "状态",
