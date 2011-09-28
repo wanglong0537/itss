1f89679c713954cd0187ac151265cc31e4b93575
muheye2006 <muheye2006@gmail.com>
2011-09-28 10:12:01 +0000

    add by guansq at 2011-9-28 修改结果集查询条件

--- 06_Office_HR/WebRoot/js/hrm/HrPostAssessmentStatusView.js
+++ 06_Office_HR/WebRoot/js/hrm/HrPostAssessmentStatusView.js
@@ -30,9 +30,9 @@ HrPostAssessmentStatusView = Ext.extend(Ext.Panel, {
 					name : "id",
 					type : "int"
 				},
-				"postApply.applyUser.fullname",
-				"postApply.postName",
-				"postApply.accessionTime",
+				"fullname",
+				"postName",
+				"accessionTime",
 				"publishStatus"
 			]
 		});
@@ -56,27 +56,6 @@ HrPostAssessmentStatusView = Ext.extend(Ext.Panel, {
 			width : 80,
 			actions : b
 		});
-		Date.prototype.format = function(format){ 
-			var o = { 
-				"M+" : this.getMonth()+1, //month 
-				"d+" : this.getDate(), //day 
-				"h+" : this.getHours(), //hour 
-				"m+" : this.getMinutes(), //minute 
-				"s+" : this.getSeconds(), //second 
-				"q+" : Math.floor((this.getMonth()+3)/3), //quarter 
-				"S" : this.getMilliseconds() //millisecond 
-			}
-			if(/(y+)/.test(format)) { 
-				format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
-			} 
-			
-			for(var k in o) { 
-				if(new RegExp("("+ k +")").test(format)) { 
-					format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length)); 
-				} 
-			}
-			return format; 
-		}
 		var c = new Ext.grid.CheckboxSelectionModel();
 		var a = new Ext.grid.ColumnModel({
 			columns : [
@@ -88,15 +67,15 @@ HrPostAssessmentStatusView = Ext.extend(Ext.Panel, {
 					hidden : true
 				}, {
 					header : "姓名",
-					dataIndex : "postApply.applyUser.fullname"
+					dataIndex : "fullname"
 				}, {
 					header : "所属岗位",
-					dataIndex : "postApply.postName"
+					dataIndex : "postName"
 				}, {
 					header : "入职时间",
-					dataIndex : "postApply.accessionTime",
+					dataIndex : "accessionTime",
 					renderer : function(c) {
-						return new Date(c).format("yyyy-MM-dd");
+						return c.split(" ")[0];
 					}
 				}, {
 					header : "状态",
