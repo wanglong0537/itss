1b5f0a5a180c0a0bcaba164c4584ef7d1cdc4b2e
muheye2006 <muheye2006@gmail.com>
2011-09-30 07:50:40 +0000

    add by guansq at 2011-9-30 修改字符编码

--- 06_Office_HR/db/hr_update_20110929.sql
+++ 06_Office_HR/db/hr_update_20110929.sql
@@ -1,7 +1,7 @@
-alter table hr_pa_kpipbc add column lineManager bigint default null comment '直接上级';
-alter table hr_pa_kpipbc2user add column lineManager bigint default null comment '直接上级';
-alter table hr_pa_kpiitem2user add column remark varchar(1000) default null comment '实际完成情况';
-alter table hr_pa_authpbcitem add column remark varchar(1000) default null comment '实际完成情况';
-alter table hr_pa_kpipbc_hist add column lineManager bigint default null comment '直接上级';
-alter table hr_pa_kpipbc2usercmp add column lineManager bigint default null comment '直接上级';
-alter table hr_pa_kpiitem2usercmp add column remark varchar(1000) default null comment '实际完成情况';
+alter table hr_pa_kpipbc add column lineManager bigint default null comment '鐩存帴涓婄骇';
+alter table hr_pa_kpipbc2user add column lineManager bigint default null comment '鐩存帴涓婄骇';
+alter table hr_pa_kpiitem2user add column remark varchar(1000) default null comment '瀹為檯瀹屾垚鎯呭喌';
+alter table hr_pa_authpbcitem add column remark varchar(1000) default null comment '瀹為檯瀹屾垚鎯呭喌';
+alter table hr_pa_kpipbc_hist add column lineManager bigint default null comment '鐩存帴涓婄骇';
+alter table hr_pa_kpipbc2usercmp add column lineManager bigint default null comment '鐩存帴涓婄骇';
+alter table hr_pa_kpiitem2usercmp add column remark varchar(1000) default null comment '瀹為檯瀹屾垚鎯呭喌';
