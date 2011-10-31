ce3fbc69b37339fb164c6b10ce3ce3644680ddaf
icemanzp <icemanzp@gmail.com>
2011-10-31 08:29:55 +0000

    Comment By Jack: 1. 增加首页样式 2. 增加常用表个

--- 07_CMS/WebRoot/r/cms/www/red/js/slide0721.js
+++ 07_CMS/WebRoot/r/cms/www/red/js/slide0721.js
@@ -1,47 +1,75 @@
-$(function(){
- $(".albumshow dl dt>a:first").addClass("tabActive");
- $(".albumshow dl dd ul").not(":first").hide();
- $(".albumshow dl dt>a").unbind("click").bind("click", function(){
-  $(this).siblings("a").removeClass("tabActive").end().addClass("tabActive");
-  var index = $(".albumshow dl dt>a").index( $(this) );
-  $(".albumshow dl dd ul").eq(index).siblings(".albumshow dl dd ul").hide().end().fadeIn("slow");
-   });
-});
 $(document).ready(function(){
- $('.albumshow dl dt a:first').addClass('tabActive');
- $('.albumshow dl dd ul:first').css('display','block');
- autoroll();
- hookThumb();
+	/* This code is executed after the DOM has been completely loaded */
+	
+	var totWidth=0;
+	var positions = new Array();
+	
+	$('#slides .slide').each(function(i){
+		
+		/* Traverse through all the slides and store their accumulative widths in totWidth */
+		
+		positions[i]= totWidth;
+		totWidth += $(this).width();
+		
+		/* The positions array contains each slide's commulutative offset from the left part of the container */
+		
+		if(!$(this).width())
+		{
+			alert("Please, fill in width & height for all your images!");
+			return false;
+		}
+		
+	});
+	
+	$('#slides').width(totWidth);
+
+	/* Change the cotnainer div's width to the exact width of all the slides combined */
+
+	$('#menu ul li a').click(function(e,keepScroll){
+
+			/* On a thumbnail click */
+
+			$('li.menuItem').removeClass('act').addClass('inact');
+			$(this).parent().addClass('act');
+			
+			var pos = $(this).parent().prevAll('.menuItem').length;
+			
+			$('#slides').stop().animate({marginLeft:-positions[pos]+'px'},450);
+			/* Start the sliding animation */
+			
+			e.preventDefault();
+			/* Prevent the default action of the link */
+			
+			
+			// Stopping the auto-advance if an icon has been clicked:
+			if(!keepScroll) clearInterval(itvl);
+	});
+	
+	$('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact');
+	/* On page load, mark the first thumbnail as active */
+	
+	
+	
+	/*****
+	 *
+	 *	Enabling auto-advance.
+	 *
+	 ****/
+	 
+	var current=1;
+	function autoAdvance()
+	{
+		if(current==-1) return false;
+		
+		$('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click',[true]);	// [true] will be passed as the keepScroll parameter of the click function on line 28
+		current++;
+	}
+
+	// The number of seconds that the slider will auto-advance in:
+	
+	var changeEvery = 3;
+
+	var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
+
+	/* End of customizations */
 });
-var i=-1;
-var offset = 3000;
-var timer = null;
-function autoroll(){
- n = $('.albumshow dl dt a').length-1;
- i++;
- if(i > n){
- i = 0;
- }
- slide(i);
-    timer = window.setTimeout(autoroll, offset);
- }
-function slide(i){
- $('.albumshow dl dt a').eq(i).addClass('tabActive').siblings().removeClass('tabActive');
- $('.albumshow dl dd ul').eq(i).fadeIn("slow").siblings('.albumshow dl dd ul').hide();
- }
-function hookThumb(){    
- $('.albumshow dl dt a').hover(
-  function () {
-    if (timer) {
-                clearTimeout(timer);
-    i = $(this).prevAll().length;
-             slide(i); 
-            }
-  },
-  function () {
-            timer = window.setTimeout(autoroll, offset);  
-            this.blur();            
-            return false;
-  }
-); 
-}
