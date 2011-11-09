ac390c5c330ed951243cee7d64f946669941afa9
muheye2006 <muheye2006@gmail.com>
2011-11-09 03:56:22 +0000

    add by guansq at 2011-11-9 添加批量导入定性考核指标功能

--- branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaPerformanceindexService.java
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/kpi/HrPaPerformanceindexService.java
@@ -1,11 +1,19 @@
 package com.xpsoft.oa.service.kpi;
 
+import java.util.List;
+import java.util.Map;
+
 import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.kpi.HrPaPerformanceindex;
+import com.xpsoft.oa.model.kpi.HrPaPerformanceindexscore;
 
 public interface HrPaPerformanceindexService extends BaseService<HrPaPerformanceindex> {
 	/*
 	 * 发布考核指标
 	 * */
 	public abstract boolean saveToPublish(long piId);
+	/*
+	 * 批量导入考核指标
+	 * */
+	public abstract boolean uploadPi(List<Map<HrPaPerformanceindex, List<HrPaPerformanceindexscore>>> list);
 }
