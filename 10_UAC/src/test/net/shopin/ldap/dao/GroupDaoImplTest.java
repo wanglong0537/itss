533253cf2d2c9b57d76908741bcb63024d8e733a
awenhaowc <awenhaowc@gmail.com>
2011-11-24 06:09:11 +0000

    add by awen

---
+++ 10_UAC/src/test/net/shopin/ldap/dao/GroupDaoImplTest.java
@@ -0,0 +1,61 @@
+package net.shopin.ldap.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.shopin.ldap.entity.Department;
+import net.shopin.ldap.entity.UserGroup;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class GroupDaoImplTest {
+	private static ApplicationContext context;
+	private static GroupDao groupDao;
+
+	@Before
+	public void setUp() throws Exception {
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		groupDao = (GroupDao) context.getBean("groupDao");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	public void testCreat() {
+		
+		UserGroup userGroup = new UserGroup();
+		userGroup.setCn("TestAdmin");
+		userGroup.setDisplayName("测试管理员");
+		userGroup.setDescription("描述信息");
+		userGroup.setStatus(1);
+		List members = new ArrayList();
+		members.add("uid=admin,ou=employees,ou=users,dc=shopin,dc=net");
+		members.add("uid=ansong,ou=employees,ou=users,dc=shopin,dc=net");
+		userGroup.setMembers(members);
+		groupDao.create(userGroup);
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		UserGroup userGroup = new UserGroup();
+		userGroup.setCn("TestAdmin");
+		userGroup.setDisplayName("测试管理员");
+		userGroup.setDescription("描述信息");
+		userGroup.setStatus(1);
+		List members = new ArrayList();
+		members.add("uid=admin,ou=employees,ou=users,dc=shopin,dc=net");
+		members.add("uid=ansong,ou=employees,ou=users,dc=shopin,dc=net");
+		userGroup.setMembers(members);
+		groupDao.update(userGroup);
+	}
+
+
+}
