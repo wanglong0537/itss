7dae05ab4874596c8965486572696657a74be7b1
awenhaowc <awenhaowc@gmail.com>
2011-11-24 12:45:13 +0000

    add by awen

---
+++ 10_UAC/src/net/shopin/ldap/entity/Duty.java
@@ -0,0 +1,109 @@
+package net.shopin.ldap.entity;
+
+import java.io.Serializable;
+
+/**
+ * Duty
+ * @author wchao
+ *
+ */
+public class Duty implements Serializable  {
+
+	private String cn;//可能没用
+	private String dn; 
+	private String title; //用户组名称 必填
+	private String description; //非必填
+	private String o;
+	private Integer status;
+	public String getCn() {
+		return cn;
+	}
+	public void setCn(String cn) {
+		this.cn = cn;
+	}
+	public String getDn() {
+		return dn;
+	}
+	public void setDn(String dn) {
+		this.dn = dn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getO() {
+		return o;
+	}
+	public void setO(String o) {
+		this.o = o;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cn == null) ? 0 : cn.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((o == null) ? 0 : o.hashCode());
+		result = prime * result + ((dn == null) ? 0 : dn.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Duty other = (Duty) obj;
+		if (cn == null) {
+			if (other.cn != null)
+				return false;
+		} else if (!cn.equals(other.cn))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (o == null) {
+			if (other.o != null)
+				return false;
+		} else if (!o.equals(other.o))
+			return false;
+		if (dn == null) {
+			if (other.dn != null)
+				return false;
+		} else if (!dn.equals(other.dn))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+}
