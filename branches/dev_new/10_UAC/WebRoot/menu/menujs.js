466b83b2b8a48840d53a4f47b10dfabecec317d9
muheye2006 <muheye2006@gmail.com>
2011-11-25 06:36:42 +0000

    add by guansq at 2011-11-25 添加功能前端界面和部分Servlet

--- branches/dev_new/10_UAC/WebRoot/menu/menujs.js
+++ branches/dev_new/10_UAC/WebRoot/menu/menujs.js
@@ -2,20 +2,20 @@
 	id : 'ou=orgnizations',
 	leaf : true,
 	text : '组织架构',
-	url : 'http://www.baidu.com'
+	url : webContext + '/user/deptManage/dept.jsp'
 }, {
 	id : 'ou=users',
 	leaf : true,
 	text : '人员架构',
-	url : 'http://www.hao123.com'
+	url : webContext + '/user/userManage/user.jsp'
 }, {
 	id : 'ou=groups',
 	leaf : true,
 	text : '组信息',
-	url : 'http://www.sina.com'
+	url : webContext + '/user/groupManage/group.jsp'
 }, {
 	id : 'ou=duties',
 	leaf : true,
 	text : '职务信息',
-	url : 'http://www.sohu.com'
+	url : webContext + '/user/postManage/post.jsp'
 } ]
