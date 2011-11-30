77c04b3f7e5f7f4ffdfbeac22f60a35e39c78b97
soyona.king <soyona.king@gmail.com>
2011-11-30 09:14:24 +0000

    提供获取管理员可访问的所有资源的接口

--- branches/dev_new/15_BandPoor/src/com/xpsoft/oa/dao/system/FunUrlDao.java
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/dao/system/FunUrlDao.java
@@ -1,11 +1,14 @@
 package com.xpsoft.oa.dao.system;
 
+import java.util.Set;
+
 import com.xpsoft.core.dao.BaseDao;
 import com.xpsoft.oa.model.system.FunUrl;
 
 public abstract interface FunUrlDao extends BaseDao<FunUrl>
 {
   public abstract FunUrl getByPathFunId(String paramString, Long paramLong);
+  public abstract Set<String> getAdminDataSource();
 }
 
 /* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
