88ba383661080c526025550c2ddc5ea348b04a9a
soyona.king <soyona.king@gmail.com>
2011-12-06 06:06:47 +0000

    修改save方法， 1.保存App_function 2.清除Fun_url 3.保存Fun_url

--- 15_BandPoor/src/com/xpsoft/oa/service/system/impl/AppFunctionServiceImpl.java
+++ 15_BandPoor/src/com/xpsoft/oa/service/system/impl/AppFunctionServiceImpl.java
@@ -1,9 +1,12 @@
 /*    */ package com.xpsoft.oa.service.system.impl;
 /*    */ 
 /*    */ import com.xpsoft.core.service.impl.BaseServiceImpl;
+import com.xpsoft.core.util.AppUtil;
 /*    */ import com.xpsoft.oa.dao.system.AppFunctionDao;
 /*    */ import com.xpsoft.oa.model.system.AppFunction;
-/*    */ import com.xpsoft.oa.service.system.AppFunctionService;
+import com.xpsoft.oa.model.system.FunUrl;
+import com.xpsoft.oa.service.system.AppFunctionService;
+import com.xpsoft.oa.service.system.FunUrlService;
 /*    */ 
 /*    */ public class AppFunctionServiceImpl extends BaseServiceImpl<AppFunction>
 /*    */   implements AppFunctionService
@@ -20,6 +23,36 @@
 /*    */   {
 /* 24 */     return this.dao.getByKey(key);
 /*    */   }
+
+			
+           public void updateFunUrl(String[] funUrls,Long functionId){
+        	FunUrlService fus =  (FunUrlService)AppUtil.getBean("funUrlService");
+       		fus.removeDatabySql("Delete from fun_url where functionId= "+functionId);
+       		for(String funurl:funUrls){
+                   FunUrl fu_ = new FunUrl();
+                   fu_.setFunctionId(functionId);
+                   fu_.setUrlPath(funurl);
+                   //fu_.setAppFunction(null);
+       			  fus.save(fu_);
+       		}
+           }
+           
+           private void removeUrlsByFunctionId(Long id){
+        	   if (id != null) {
+					removeDatabySql("Delete from fun_url where functionId= "+id);
+       		}
+           }
+           
+           private void removeAppFunctionById(Long id){
+        	   if (id != null) {
+       				removeDatabySql("Delete from app_function where functionId= "+id);
+       		}
+           }
+           public void remove(Long id){
+        	   removeUrlsByFunctionId(id);
+        	   removeAppFunctionById(id);
+           }
+           
 /*    */ }
 
 /* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
