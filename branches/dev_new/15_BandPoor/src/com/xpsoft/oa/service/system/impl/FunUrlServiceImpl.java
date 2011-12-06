8b188e8ccd9c6f50dc23f126d5b868ec91009166
soyona.king <soyona.king@gmail.com>
2011-12-06 06:07:27 +0000

    //comment

--- branches/dev_new/15_BandPoor/src/com/xpsoft/oa/service/system/impl/FunUrlServiceImpl.java
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/service/system/impl/FunUrlServiceImpl.java
@@ -31,12 +31,4 @@ import com.xpsoft.oa.service.system.FunUrlService;
 				// TODO Auto-generated method stub
 				return null;//this.dao.getByFunId();
 			}
-           
-           
-           
-/*    */ }
-
-/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
- * Qualified Name:     com.xpsoft.oa.service.system.impl.FunUrlServiceImpl
- * JD-Core Version:    0.6.0
- */
+}          
