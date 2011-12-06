4238cd4774af630f8d7309dc793cafa7dd6d7398
soyona.king <soyona.king@gmail.com>
2011-12-06 06:08:59 +0000

    //comment

--- 15_BandPoor/src/com/xpsoft/oa/dao/system/FunUrlDao.java
+++ 15_BandPoor/src/com/xpsoft/oa/dao/system/FunUrlDao.java
@@ -11,7 +11,3 @@ public abstract interface FunUrlDao extends BaseDao<FunUrl>
   public abstract Set<String> getAdminDataSource();
 }
 
-/* Location:           C:\Users\Jack\Downloads\oa\joffice131Tomcat6\joffice131Tomcat6\tomcat6-joffice\webapps\joffice1.3.1\WEB-INF\classes\
- * Qualified Name:     com.xpsoft.oa.dao.system.FunUrlDao
- * JD-Core Version:    0.6.0
- */
