16aa3e44ceada648c3f899d97132ca33584bd6ec
muheye2006 <muheye2006@gmail.com>
2011-12-06 06:40:29 +0000

    add by guansq at 2011-12-06 添加品牌管理和品类管理模块

---
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/service/bandpoor/ProClassService.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.service.bandpoor;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.bandpoor.ProClass;
+
+public abstract interface ProClassService extends BaseService<ProClass>{
+
+}
