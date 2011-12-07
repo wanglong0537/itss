db984b75332a6bd96c6e4693a3e749f1f90f75eb
muheye2006 <muheye2006@gmail.com>
2011-12-07 06:20:29 +0000

    add by guansq at 2011-12-07 添加主力价格模块

---
+++ branches/dev_new/15_BandPoor/WebRoot/js/bandpoor/MainPriceForm.js
@@ -0,0 +1,107 @@
+MainPriceForm = Ext.extend(Ext.Window, {
+	formPanel : null,
+	constructor : function(a) {
+		if(a == null) {
+			a = {};
+		}
+		Ext.apply(this, a);
+		this.initComponents();
+		MainPriceForm.superclass.constructor.call(this, {
+			id : "MainPriceFormWin",
+			layout : "fit",
+			items : this.formPanel,
+			modal : true,
+			height : 180,
+			width : 350,
+			title : "主力价格详细信息",
+			buttonAlign : "center",
+			buttons : this.buttons
+		});
+	},
+	initComponents : function() {
+		this.formPanel = new Ext.FormPanel({
+			layout : "form",
+			bodyStyle : "padding:10px 10px 10px 10px",
+			border : false,
+			url : __ctxPath + "/bandpoor/saveMainPrice.do",
+			id : "MainPriceForm",
+			defaults : {
+				anchor : "95%,95%"
+			},
+			defaultType : "textfield",
+			items : [
+				{
+					name : "mainPrice.id",
+					id : "mainPriceId",
+					xtype : "hidden",
+					value : this.mainPriceId == null ? "" : this.mainPriceId
+				}, {
+					fieldLabel : "名称",
+					name : "mainPrice.priceName",
+					id : "priceName",
+					allowBlank : false,
+					blankText : "名称不能为空！"
+				}, {
+					fieldLabel : "描述",
+					name : "mainPrice.priceDesc",
+					id : "priceDesc",
+					xtype : "textarea"
+				}
+			]
+		});
+		if(this.mainPriceId != null && this.mainPriceId != "undefined") {
+			this.formPanel.getForm().load({
+				deferredRender : false,
+				url : __ctxPath + "/bandpoor/getMainPrice.do?id=" + this.mainPriceId,
+				waitMsg : "正在载入数据……",
+				success : function(f, d) {
+					
+				},
+				failure : function() {
+					
+				}
+			});
+		}
+		this.buttons = [
+			{
+				text : "保存",
+				iconCls : "btn-save",
+				handler : this.save.createCallback(this.formPanel, this)
+			}, {
+				text : "取消",
+				iconCls : "btn-cancel",
+				handler : this.cancel.createCallback(this)
+			}
+		];
+	},
+	cancel : function(a) {
+		a.close();
+	},
+	save : function(a, b) {
+		if(a.getForm().isValid()) {
+			a.getForm().submit({
+				method : "post",
+				waitMsg : "正在提交数据……",
+				success : function() {
+					Ext.ux.Toast.msg("提示信息","保存成功！");
+					Ext.getCmp("MainPriceView").gridPanel.store.reload({
+						params : {
+							start : 0,
+							limit : 25
+						}
+					});
+					b.close();
+				},
+				failure : function(c, d) {
+					Ext.MessageBox.show({
+						title : "操作信息",
+						msg : d.result.msg,
+						buttons : Ext.MessageBox.OK,
+						icon : Ext.MessageBox.ERROR
+					});
+					return ;
+				}
+			});
+		}
+	}
+});
