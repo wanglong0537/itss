366ec5244a40eca3b59c548f3ca3da1e2d16651e
muheye2006 <muheye2006@gmail.com>
2011-12-09 09:44:17 +0000

    add by guansq at 2011-12-09 添加品牌批量导入功能

--- 15_BandPoor/src/com/xpsoft/oa/service/bandpoor/FloorService.java
+++ 15_BandPoor/src/com/xpsoft/oa/service/bandpoor/FloorService.java
@@ -1,10 +1,13 @@
 package com.xpsoft.oa.service.bandpoor;
 
+import java.util.List;
 import java.util.Map;
 
 import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.bandpoor.BandStyle;
 import com.xpsoft.oa.model.bandpoor.Floor;
 
 public abstract interface FloorService extends BaseService<Floor>{
 	public boolean validateUnique(Map params);
+	public boolean multiSave(List<Floor> list);
 }
--- 15_BandPoor/src/com/xpsoft/oa/service/bandpoor/ProClassService.java
+++ 15_BandPoor/src/com/xpsoft/oa/service/bandpoor/ProClassService.java
@@ -1,9 +1,12 @@
 package com.xpsoft.oa.service.bandpoor;
 
+import java.util.List;
 import java.util.Map;
 
 import com.xpsoft.core.service.BaseService;
 import com.xpsoft.oa.model.bandpoor.ProClass;
 
 public abstract interface ProClassService extends BaseService<ProClass>{
-	public boolean validateUnique(Map params);}
+	public boolean validateUnique(Map params);
+	public boolean multiSave(List<ProClass> list);
+}
