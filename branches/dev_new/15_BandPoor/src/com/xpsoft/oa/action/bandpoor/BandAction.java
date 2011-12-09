47fb3fa14014b5955c02b798736b99efb9c2eac9
muheye2006 <muheye2006@gmail.com>
2011-12-09 04:03:45 +0000

    add by guansq at 2011-12-09 添加品牌批量导入功能

--- branches/dev_new/15_BandPoor/src/com/xpsoft/oa/action/bandpoor/BandAction.java
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/action/bandpoor/BandAction.java
@@ -1,12 +1,20 @@
 package com.xpsoft.oa.action.bandpoor;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
+
+import jxl.Sheet;
+import jxl.Workbook;
+
 import com.xpsoft.core.command.QueryFilter;
+import com.xpsoft.core.util.AppUtil;
 import com.xpsoft.core.web.action.BaseAction;
 import com.xpsoft.oa.model.bandpoor.Band;
 import com.xpsoft.oa.service.bandpoor.BandService;
@@ -96,6 +104,97 @@ public class BandAction extends BaseAction {
 		} catch(Exception e) {
 			this.jsonString = "{success:false}";
 			e.printStackTrace();
+		}
+		
+		return "success";
+	}
+	
+	public String upload() {
+		String filePath = this.getRequest().getParameter("filePath");
+		List<Band> list = new ArrayList<Band>();
+		String defaultProfix = String.valueOf(AppUtil.getSysConfig().get("file.upload.default.perfix"));
+		int len = defaultProfix.length();
+		filePath = filePath.substring(filePath.indexOf(defaultProfix));
+		File file = new File(this.getRequest().getRealPath("/") + filePath);
+		try {
+			Workbook book = Workbook.getWorkbook(file);
+			Sheet sheet = book.getSheet(0);
+			int col = sheet.getColumns();
+			int row = sheet.getRows();
+			for(int i = 2; i < row; i++) {
+				Band band1 = new Band();
+				if(StringUtils.isEmpty(sheet.getCell(0, i).getContents()) && StringUtils.isEmpty(sheet.getCell(1, i).getContents())) {
+					this.jsonString = "{success:true,flag:'0',msg:'excel中存在既没有中文名称又没有英文名称的数据，请核实！'}";
+					return "success";
+				}
+				if(StringUtils.isNotEmpty(sheet.getCell(0, i).getContents())) {
+					band1.setBandChName(sheet.getCell(0, i).getContents());
+				}
+				if(StringUtils.isNotEmpty(sheet.getCell(1, i).getContents())) {
+					band1.setBandEnName(sheet.getCell(1, i).getContents());
+				}
+				if("0".equals(sheet.getCell(2, i).getContents())) {
+					band1.setBandStatus(0);
+				} else if("1".equals(sheet.getCell(2, i).getContents())) {
+					band1.setBandStatus(1);
+				} else {
+					this.jsonString = "{success:true,flag:'0',msg:'excel中存在类型不符合要求的数据，请核实！'}";
+					return "success";
+				}
+				band1.setBandDesc(sheet.getCell(3, i).getContents());
+				band1.setFlag(Band.CREATE);
+				list.add(band1);
+			}
+			List<Band> list2 = list;
+			for(int i = 0; i < list.size(); i++) {
+				Band band1 = list.get(i);
+				for(int j = i + 1; j < list2.size(); j ++) {
+					Band band2 = list2.get(j);
+					if(StringUtils.isNotEmpty(band1.getBandChName())) {
+						if(band1.getBandChName().equals(band2.getBandChName())) {
+							this.jsonString = "{success:true,flag:'0',msg:'excel中存在中文名称相同的数据，请核实！'}";
+							return "success";
+						}
+					}
+					if(StringUtils.isNotEmpty(band1.getBandEnName())) {
+						if(band1.getBandEnName().equals(band2.getBandEnName())) {
+							this.jsonString = "{success:true,flag:'0',msg:'excel中存在英文名称相同的数据，请核实！'}";
+							return "success";
+						}
+					}
+				}
+				boolean flag = true;
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("Q_id_L_NEQ", "0");
+				if(StringUtils.isNotEmpty(band1.getBandChName())) {
+					map.put("Q_bandChName_S_EQ", band1.getBandChName());
+					flag = this.bandService.validateUnique(map);
+					if(!flag) {
+						this.jsonString = "{failure:true,flag:'0',msg:'中文名称【" + band1.getBandChName() + "】在数据库中已存在，请核实！'}";
+						return "success";
+					}
+					map.remove("Q_bandChName_S_EQ");
+				}
+				if(StringUtils.isNotEmpty(band1.getBandEnName())) {
+					map.put("Q_bandEnName_S_EQ", band1.getBandEnName());
+					flag = this.bandService.validateUnique(map);
+					if(!flag) {
+						this.jsonString = "{success:true,flag:'0',msg:'英文名称【" + band1.getBandEnName() + "】在数据库中已存在，请核实！'}";
+						return "success";
+					}
+				}
+			}
+			boolean result = this.bandService.multiSave(list);
+			if(result) {
+				this.jsonString = "{success:true,flag:'1'}";
+			} else {
+				this.jsonString = "{success:true,flag:'0',msg:'导入失败，请核实文件和数据！'}";
+				return "success";
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			this.jsonString = "{success:true,flag:'0',msg:'导入失败，请核实文件和数据！'}";
+			return "success";
 		}
 		
 		return "success";
