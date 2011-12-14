edee02aa1a0de0e9bfc6abdf01bed4e320b06a5d
muheye2006 <muheye2006@gmail.com>
2011-12-14 05:55:28 +0000

    add by guansq at 2011-12-14 增加品牌、品类与上品的关联

---
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/dao/bandpoor/BpShopinBandDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.bandpoor;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.bandpoor.BpShopinBand;
+
+public abstract interface BpShopinBandDao extends BaseDao<BpShopinBand>{
+
+}
