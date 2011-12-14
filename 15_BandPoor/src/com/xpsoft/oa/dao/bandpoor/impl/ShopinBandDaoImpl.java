1a432d6012370ba1026d16b927ce7b20e3a479cd
muheye2006 <muheye2006@gmail.com>
2011-12-14 05:55:40 +0000

    add by guansq at 2011-12-14 增加品牌、品类与上品的关联

---
+++ 15_BandPoor/src/com/xpsoft/oa/dao/bandpoor/impl/ShopinBandDaoImpl.java
@@ -0,0 +1,11 @@
+package com.xpsoft.oa.dao.bandpoor.impl;
+
+import com.xpsoft.core.dao.impl.BaseDaoImpl;
+import com.xpsoft.oa.dao.bandpoor.ShopinBandDao;
+import com.xpsoft.oa.model.bandpoor.ShopinBand;
+
+public class ShopinBandDaoImpl extends BaseDaoImpl<ShopinBand> implements ShopinBandDao{
+	public ShopinBandDaoImpl() {
+		super(ShopinBand.class);
+	}
+}
---
+++ 15_BandPoor/src/com/xpsoft/oa/dao/bandpoor/impl/ShopinProClassDaoImpl.java
@@ -0,0 +1,11 @@
+package com.xpsoft.oa.dao.bandpoor.impl;
+
+import com.xpsoft.core.dao.impl.BaseDaoImpl;
+import com.xpsoft.oa.dao.bandpoor.ShopinProClassDao;
+import com.xpsoft.oa.model.bandpoor.ShopinProClass;
+
+public class ShopinProClassDaoImpl extends BaseDaoImpl<ShopinProClass> implements ShopinProClassDao{
+	public ShopinProClassDaoImpl() {
+		super(ShopinProClass.class);
+	}
+}
