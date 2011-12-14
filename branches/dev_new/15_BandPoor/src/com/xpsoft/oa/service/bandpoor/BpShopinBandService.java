157b6aac7ab21c0c12e769dba86e853d4efafb74
muheye2006 <muheye2006@gmail.com>
2011-12-14 05:55:53 +0000

    add by guansq at 2011-12-14 增加品牌、品类与上品的关联

---
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/service/bandpoor/BpShopinBandService.java
@@ -0,0 +1,9 @@
+package com.xpsoft.oa.service.bandpoor;
+
+import com.xpsoft.oa.model.bandpoor.BpShopinBand;
+
+import com.xpsoft.core.service.BaseService;
+
+public abstract interface BpShopinBandService extends BaseService<BpShopinBand>{
+
+}
