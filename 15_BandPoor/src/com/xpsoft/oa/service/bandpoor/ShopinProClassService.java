ed2c05ca5d334ce00f081872370f1997e2eade53
muheye2006 <muheye2006@gmail.com>
2011-12-14 05:55:53 +0000

    add by guansq at 2011-12-14 增加品牌、品类与上品的关联

---
+++ 15_BandPoor/src/com/xpsoft/oa/service/bandpoor/ShopinProClassService.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.service.bandpoor;
+
+import com.xpsoft.core.service.BaseService;
+import com.xpsoft.oa.model.bandpoor.ShopinProClass;
+
+public abstract interface ShopinProClassService extends BaseService<ShopinProClass>{
+
+}
