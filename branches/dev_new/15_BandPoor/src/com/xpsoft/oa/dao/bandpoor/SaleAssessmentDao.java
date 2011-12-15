6f066fe3ce0906b5ecb36a6c09d8bc7037f3780a
muheye2006 <muheye2006@gmail.com>
2011-12-15 09:53:36 +0000

    add by guansq at 2011-12-15 增加”不可评分待选池考核“和”不可评分品牌池管理“模块

---
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/dao/bandpoor/SaleAssessmentDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.bandpoor;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.bandpoor.SaleAssessment;
+
+public abstract interface SaleAssessmentDao extends BaseDao<SaleAssessment>{
+
+}
---
+++ branches/dev_new/15_BandPoor/src/com/xpsoft/oa/dao/bandpoor/SaleAssessmentForBPDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.bandpoor;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.bandpoor.SaleAssessmentForBP;
+
+public abstract interface SaleAssessmentForBPDao extends BaseDao<SaleAssessmentForBP>{
+
+}
