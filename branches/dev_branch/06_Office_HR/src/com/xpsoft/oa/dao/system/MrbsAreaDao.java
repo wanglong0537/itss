232b5479ffb0e7dac22577d713eea95a46e9257f
muheye2006 <muheye2006@gmail.com>
2011-12-23 06:17:43 +0000

    add by guansq at 2011-12-23 添加会议室预定模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/system/MrbsAreaDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.system;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.system.MrbsArea;
+
+public abstract interface MrbsAreaDao extends BaseDao<MrbsArea>{
+
+}
---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/dao/system/MrbsScheduleDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.system;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.system.MrbsSchedule;
+
+public abstract interface MrbsScheduleDao extends BaseDao<MrbsSchedule>{
+
+}
