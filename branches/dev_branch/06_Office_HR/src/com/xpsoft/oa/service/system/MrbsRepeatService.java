206c7dcff290c2a12ddeee5241bd27d70f817aa7
muheye2006 <muheye2006@gmail.com>
2011-12-23 06:19:13 +0000

    add by guansq at 2011-12-23 添加会议室预定模块

---
+++ branches/dev_branch/06_Office_HR/src/com/xpsoft/oa/service/system/MrbsRepeatService.java
@@ -0,0 +1,9 @@
+package com.xpsoft.oa.service.system;
+
+import com.xpsoft.oa.model.system.MrbsRepeat;
+
+import com.xpsoft.core.service.BaseService;
+
+public abstract interface MrbsRepeatService extends BaseService<MrbsRepeat>{
+
+}
