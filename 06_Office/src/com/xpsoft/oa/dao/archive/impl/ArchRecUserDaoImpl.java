30ab5d28b485a655341ee621748e335ab5601d71
tongjunper <tongjunper@3e7974d8-cab7-0ae4-a5ba-ebead40fec54>
2011-12-31 09:23:52 +0000

    修改获取全部部门时排序

--- 06_Office/src/com/xpsoft/oa/dao/archive/impl/ArchRecUserDaoImpl.java
+++ 06_Office/src/com/xpsoft/oa/dao/archive/impl/ArchRecUserDaoImpl.java
@@ -3,7 +3,10 @@
 /*    */ import com.xpsoft.core.dao.impl.BaseDaoImpl;
 /*    */ import com.xpsoft.oa.dao.archive.ArchRecUserDao;
 /*    */ import com.xpsoft.oa.model.archive.ArchRecUser;
-/*    */ import java.util.List;
+import com.xpsoft.oa.model.system.Department;
+
+import java.util.ArrayList;
+import java.util.List;
 /*    */ 
 /*    */ public class ArchRecUserDaoImpl extends BaseDaoImpl<ArchRecUser>
 /*    */   implements ArchRecUserDao
@@ -15,10 +18,33 @@
 /*    */ 
 /*    */   public List findDepAll()
 /*    */   {
-/* 20 */     String hql = "select ar,dp from ArchRecUser ar right join ar.department dp";
-/* 21 */     return findByHql(hql);
-/*    */   }
-/*    */ 
+			List list=new ArrayList();
+/* 20 */    String hql = "select ar,dp from ArchRecUser ar right join ar.department dp where dp.parentId=0";
+			List listp=findByHql(hql);
+			for(int i = 0; i < listp.size(); i++){
+				Department dep = (Department) ((Object[]) listp.get(i))[1];
+				list.add(listp.get(i));
+				List clist=findChildDep(dep.getDepId());
+				if(clist!=null&&clist.size()>0){
+					list.addAll(clist);
+				}
+			}	
+/* 21 */     return list;
+/*    */   }	
+			private List findChildDep(Long depId){
+				List list=new ArrayList();
+				String hql = "select ar,dp from ArchRecUser ar right join ar.department dp where dp.parentId="+depId;
+				List listp=findByHql(hql);
+				for(int i = 0; i < listp.size(); i++){
+					Department dep = (Department) ((Object[]) listp.get(i))[1];
+					list.add(listp.get(i));
+					List clist=findChildDep(dep.getDepId());
+					if(clist!=null&&clist.size()>0){
+						list.addAll(clist);
+					}
+				}	
+				return list;
+			}
 /*    */   public ArchRecUser getByDepId(Long depId)
 /*    */   {
 /* 26 */     String hql = "from ArchRecUser ar where ar.department.depId =?";
