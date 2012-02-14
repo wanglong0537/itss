1127ce6607e06936f4a085d83c82c1323021f85f
muheye2006 <muheye2006@gmail.com>
2012-02-14 05:14:44 +0000

    add by guansq at 2012-02-13 新建“供应商报表后台管理系统”代码分支。

---
+++ 17_SwdManage/src/com/xpsoft/oa/dao/miswap/TmSendDao.java
@@ -0,0 +1,8 @@
+package com.xpsoft.oa.dao.miswap;
+
+import com.xpsoft.core.dao.BaseDao;
+import com.xpsoft.oa.model.miswap.TmSend;
+
+public abstract interface TmSendDao extends BaseDao<TmSend>{
+
+}
