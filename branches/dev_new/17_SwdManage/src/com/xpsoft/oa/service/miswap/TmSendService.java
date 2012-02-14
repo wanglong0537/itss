534b608760b15ee1fef33ede00b07923efe5c5c1
muheye2006 <muheye2006@gmail.com>
2012-02-14 05:15:35 +0000

    add by guansq at 2012-02-13 新建“供应商报表后台管理系统”代码分支。

---
+++ branches/dev_new/17_SwdManage/src/com/xpsoft/oa/service/miswap/TmSendService.java
@@ -0,0 +1,9 @@
+package com.xpsoft.oa.service.miswap;
+
+import com.xpsoft.oa.model.miswap.TmSend;
+
+import com.xpsoft.core.service.BaseService;
+
+public abstract interface TmSendService extends BaseService<TmSend>{
+
+}
