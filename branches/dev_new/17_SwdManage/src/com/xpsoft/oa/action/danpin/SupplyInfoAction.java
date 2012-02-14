2c1813ca289ed73438a58fd5d59a91362ddcfbd8
muheye2006 <muheye2006@gmail.com>
2012-02-14 05:14:13 +0000

    add by guansq at 2012-02-13 新建“供应商报表后台管理系统”代码分支。

--- branches/dev_new/17_SwdManage/src/com/xpsoft/oa/action/danpin/SupplyInfoAction.java
+++ branches/dev_new/17_SwdManage/src/com/xpsoft/oa/action/danpin/SupplyInfoAction.java
@@ -1,12 +1,21 @@
 package com.xpsoft.oa.action.danpin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.xpsoft.core.command.QueryFilter;
 import com.xpsoft.core.web.action.BaseAction;
 import com.xpsoft.oa.model.danpin.SupplyInfo;
 import com.xpsoft.oa.service.danpin.SupplyInfoService;
 
+import flexjson.JSONSerializer;
+
 public class SupplyInfoAction extends BaseAction{
 	private Long sid;
 	private SupplyInfo supplyInfo;
+	@Resource
 	private SupplyInfoService supplyInfoService;
 	
 	public Long getSid() {
@@ -26,5 +35,19 @@ public class SupplyInfoAction extends BaseAction{
 	}
 	public void setSupplyInfoService(SupplyInfoService supplyInfoService) {
 		this.supplyInfoService = supplyInfoService;
+	}
+	
+	public String list() {
+		QueryFilter filter = new QueryFilter(this.getRequest());
+		List<SupplyInfo> list = this.supplyInfoService.getAll(filter);
+		
+		StringBuffer buff = new StringBuffer("{success:true,'totalCounts':")
+				.append(filter.getPagingBean().getTotalItems()).append(",result:");
+		JSONSerializer json = new JSONSerializer();
+		buff.append(json.exclude(new String[] {}).serialize(list));
+		buff.append("}");
+		this.jsonString = buff.toString();
+		
+		return "success";
 	}
 }
