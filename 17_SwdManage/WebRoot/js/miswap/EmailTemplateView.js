daf3a99c0e15f1450639342efde78c9050c29b4b
muheye2006 <muheye2006@gmail.com>
2012-02-14 05:16:39 +0000

    add by guansq at 2012-02-13 新建“供应商报表后台管理系统”代码分支。

---
+++ 17_SwdManage/WebRoot/js/miswap/EmailTemplateView.js
@@ -0,0 +1,162 @@
+EmailTemplateView = Ext.extend(Ext.Panel, {
+	constructor : function(a) {
+		if(a == null) {
+			a = {};
+		}
+		Ext.apply(this, a);
+		this.initComponents();
+		EmailTemplateView.superclass.constructor.call(this, {
+			id : "EmailTemplateView",
+			title : "邮件模板管理",
+			region : "center",
+			layout : "border",
+			items : [
+				this.searchPanel,
+				this.gridPanel
+			]
+		});
+	},
+	searchPanel : null,
+	gridPanel : null,
+	store : null,
+	topbar : null,
+	initComponents : function() {
+		this.searchPanel = new Ext.FormPanel({
+			region : "north",
+			height : 40,
+			frame : false,
+			border : false,
+			layout : "hbox",
+			layoutConfig : {
+				padding : "5",
+				align : "middle"
+			},
+			defaults : {
+				xtype : "label",
+				margins : {
+					top : 0,
+					right : 4,
+					bottom : 4,
+					left : 4
+				}
+			},
+			items : [
+				{
+					text : "查询条件：模板名称"
+				}, {
+					fieldLabel : "模板名称",
+					name : "Q_name_S_LK",
+					xtype : "textfield"
+				}, {
+					xtype : "button",
+					text : "查询",
+					handler : this.search.createCallback(this)
+				}
+			]
+		});
+		this.store = new Ext.data.JsonStore({
+			url : __ctxPath + "/miswap/listEmailTemplate.do",
+			totalProperty : "totalCounts",
+			id : "id",
+			root : "result",
+			remoteSort : true,
+			fields : [
+				{
+					name : "id",
+					type : "int"
+				},
+				"name",
+				"description"
+			]
+		});
+		this.store.setDefaultSort("id", "desc");
+		this.store.load({
+			params : {
+				start : 0,
+				limit : 25
+			}
+		});
+		var b = new Array();
+		b.push({
+			iconCls : "btn-preview",
+			qtip : "预览",
+			style : "margin:0 3px 0 3px"
+		});
+		this.rowActions = new Ext.ux.grid.RowActions({
+			header : "管理",
+			width : 80,
+			actions : b
+		});
+		var c = new Ext.grid.CheckboxSelectionModel();
+		var a = new Ext.grid.ColumnModel({
+			columns : [
+				c,
+				new Ext.grid.RowNumberer(),
+				{
+					header : "id",
+					dataIndex : "id",
+					hidden : true
+				}, {
+					header : "模板名称",
+					dataIndex : "name"
+				}, {
+					header : "描述",
+					dataIndex : "description"
+				},
+				this.rowActions
+			],
+			defaults : {
+				sortable : true,
+				menuDisable : false,
+				width : 100
+			}
+		});
+		this.gridPanel = new Ext.grid.GridPanel({
+			id : "EmailTemplateGrid",
+			region : "center",
+			autoWidth : true,
+			autoHeight : true,
+			stripeRows : true,
+			closeable : true,
+			store : this.store,
+			trackMouseOver : true,
+			disableSelection : false,
+			loadMask : true,
+			cm : a,
+			sm : c,
+			plugins : this.rowActions,
+			viewConfig : {
+				forceFit : true,
+				enableRowBody : false,
+				showPreview : false
+			},
+			bbar : new Ext.PagingToolbar({
+				pageSize : 25,
+				store : this.store,
+				displayInfo : true,
+				displayMsg : "当前显示{0}至{1}，共{2}条记录",
+				emptyMsg : "当前没有记录"
+			})
+		});
+		this.gridPanel.addListener("rowdblclick", function(f, d, g) {
+			f.getSelectionModel().each(function(e) {
+				new EmailTemplateForm({
+					floorId : e.data.id
+				}).show();
+			});
+		});
+		this.rowActions.on("action", this.onRowAction, this);
+	},
+	search : function(a) {
+		if(a.searchPanel.getForm().isValid()) {
+			a.searchPanel.getForm().submit({
+				waitMsg : "正在提交查询……",
+				url : __ctxPath + "/miswap/listEmailTemplate.do",
+				success : function(c, d) {
+					var e = Ext.util.JSON.decode(d.response.responseText);
+					a.gridPanel.getStore().loadData(e);
+				}
+			});
+		}
+	}
+});
