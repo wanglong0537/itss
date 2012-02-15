610eb28d0c598e31b4ebea6aa6f147eedd697404
muheye2006 <muheye2006@gmail.com>
2012-02-15 01:34:19 +0000

    add by guansq at 2012-02-13 新建“供应商报表后台管理系统”代码分支。

--- 17_SwdManage/src/com/xpsoft/oa/action/miswap/SupplyConfigAction.java
+++ 17_SwdManage/src/com/xpsoft/oa/action/miswap/SupplyConfigAction.java
@@ -1,10 +1,22 @@
 package com.xpsoft.oa.action.miswap;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import com.xpsoft.core.command.QueryFilter;
+import com.xpsoft.core.util.AppUtil;
 import com.xpsoft.core.web.action.BaseAction;
+import com.xpsoft.oa.model.danpin.SupplyInfo;
+import com.xpsoft.oa.model.miswap.EmailTemplate;
 import com.xpsoft.oa.model.miswap.SupplyConfig;
+import com.xpsoft.oa.model.miswap.TmTemplate;
+import com.xpsoft.oa.service.danpin.SupplyInfoService;
+import com.xpsoft.oa.service.miswap.EmailTemplateService;
 import com.xpsoft.oa.service.miswap.SupplyConfigService;
+import com.xpsoft.oa.service.miswap.TmTemplateService;
+
+import flexjson.JSONSerializer;
 
 public class SupplyConfigAction extends BaseAction{
 	private Long id;
@@ -34,6 +46,48 @@ public class SupplyConfigAction extends BaseAction{
 	public String save() {
 		this.supplyConfigService.save(this.supplyConfig);
 		this.jsonString = "{success:true}";
+		
+		return "success";
+	}
+	
+	public String get() {
+		SupplyInfoService supplyInfoService = (SupplyInfoService)AppUtil.getBean("supplyInfoService");
+		EmailTemplateService emailTemplateService = (EmailTemplateService)AppUtil.getBean("emailTemplateService");
+		TmTemplateService tmTemplateService = (TmTemplateService)AppUtil.getBean("tmTemplateService");
+		
+		Long supplyInfoSid = Long.parseLong(this.getRequest().getParameter("Q_supplyInfoSid_L_EQ"));
+		SupplyInfo supplyInfo = supplyInfoService.get(supplyInfoSid);
+		EmailTemplate et = new EmailTemplate();
+		TmTemplate tt = new TmTemplate();
+		
+		QueryFilter filter = new QueryFilter(this.getRequest());
+		List<SupplyConfig> list = this.supplyConfigService.getAll(filter);
+		if(list.size() > 0) {
+			this.supplyConfig = list.get(0);
+			if(this.supplyConfig.getEmailId() != null) {
+				et = emailTemplateService.get(this.supplyConfig.getEmailId());
+			}
+			if(this.supplyConfig.getTmId() != null) {
+				tt = tmTemplateService.get(this.supplyConfig.getTmId());
+			}
+		}
+		
+		
+		JSONSerializer json = new JSONSerializer();
+		StringBuffer buff = new StringBuffer("{success:true,data:");
+		buff.append("{")
+				.append("'id':'" + (this.supplyConfig == null ? "" : this.supplyConfig.getId()) + "',")
+				.append("'supplyInfoSid':'" + supplyInfo.getSid() + "',")
+				.append("'supplyId':'" + supplyInfo.getSupplyId() + "',")
+				.append("'receiveTm':'" + (this.supplyConfig == null ? "" : this.supplyConfig.getReceiveTm()) + "',")
+				.append("'tmId':'" + (tt.getId() == null ? "" : tt.getId()) + "',")
+				.append("'tmName':'" + (tt.getName() == null ? "" : tt.getName()) + "',")
+				.append("'receiveEmail':'" + (this.supplyConfig == null ? "" : this.supplyConfig.getReceiveEmail()) + "',")
+				.append("'emailId':'" + (et.getId() == null ? "" : et.getId()) + "',")
+				.append("'emailName':'" + (et.getName() == null ? "" : et.getName()) + "'")
+				.append("}");
+		buff.append("}");
+		this.jsonString = buff.toString();
 		
 		return "success";
 	}
