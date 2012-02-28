8b1b5e745f9148cb7fa085002bb8f30cbb5722c1
tongjunper <tongjunper@gmail.com>
2012-02-28 09:29:51 +0000

    提交供应商信息功能

---
+++ branches/dev_new/14_dataMonitor/src/com/xp/monitor/bean/UpLoadFile.java
@@ -0,0 +1,57 @@
+package com.xp.monitor.bean;
+
+import java.util.Date;
+
+public class UpLoadFile {
+	private Long id;
+	
+	private String realName;
+	
+	private String filePath;
+	
+	private Date createDate;
+	
+	private String createUser;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	
+	
+}
